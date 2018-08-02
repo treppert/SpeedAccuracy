5ed3805b22bf1de20915b0e77a1c30da4044395b
treppert <thomas.reppert@gmail.com>
2018-08-02 17:20:10 -0500

    Modified fxn plot_cdf_vigor_SAT() such that it now plots distributions by both trial type (Fast vs Accurate) and error (correct choice vs. mis-directed choice).

--- Vigor/plot_cdf_vigor_SAT.m
+++ Vigor/plot_cdf_vigor_SAT.m
@@ -5,6 +5,9 @@ function [  ] = plot_cdf_vigor_SAT( moves , info )
 idx_acc = ([info.condition] == 1);
 idx_fast = ([info.condition] == 3);
 
+idx_corr = ~([info.err_dir] | [info.err_time]);
+idx_errdir = [info.err_dir];
+
 vigor = [moves.vigor];
 
 %remove NaNs
@@ -12,16 +15,24 @@ i_nan = isnan(vigor);
 vigor(i_nan) = [];
 idx_acc(i_nan) = [];
 idx_fast(i_nan) = [];
+idx_corr(i_nan) = [];
+idx_errdir(i_nan) = [];
 
-vig_acc = sort(vigor(idx_acc));
-vig_fast = sort(vigor(idx_fast));
+vig_corr_A = sort(vigor(idx_corr & idx_acc));
+vig_corr_F = sort(vigor(idx_corr & idx_fast));
+vig_errdir_A = sort(vigor(idx_errdir & idx_acc));
+vig_errdir_F = sort(vigor(idx_errdir & idx_fast));
 
-y_acc = (1:sum(idx_acc)) / sum(idx_acc);
-y_fast = (1:sum(idx_fast)) / sum(idx_fast);
+y_corr_A = (1:sum(idx_corr & idx_acc)) / sum(idx_corr & idx_acc);
+y_corr_F = (1:sum(idx_corr & idx_fast)) / sum(idx_corr & idx_fast);
+y_errdir_A = (1:sum(idx_errdir & idx_acc)) / sum(idx_errdir & idx_acc);
+y_errdir_F = (1:sum(idx_errdir & idx_fast)) / sum(idx_errdir & idx_fast);
 
 figure(); hold on
-plot(vig_acc, y_acc, 'r-', 'LineWidth',1.25)
-plot(vig_fast, y_fast, '-', 'Color',[0 .7 0], 'LineWidth',1.25)
+plot(vig_corr_A, y_corr_A, 'r-')
+plot(vig_corr_F, y_corr_F, '-', 'Color',[0 .7 0])
+plot(vig_errdir_A, y_errdir_A, 'r--', 'LineWidth',1.75)
+plot(vig_errdir_F, y_errdir_F, '--', 'Color',[0 .7 0], 'LineWidth',1.75)
 ppretty()
 
 end%fxn:plot_cdf_vigor_SAT()
