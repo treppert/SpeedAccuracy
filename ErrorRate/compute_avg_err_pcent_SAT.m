ae96f2b37594cd8a515052ccf23165ce0ffa05bf
treppert <thomas.reppert@gmail.com>
2018-08-03 16:53:17 -0500

    Added fxn compute_avg_vigor_SAT() to provide estimates of mean vigor X condition X error.

--- ErrorRate/compute_avg_err_pcent_SAT.m
+++ ErrorRate/compute_avg_err_pcent_SAT.m
@@ -8,6 +8,8 @@ err_dir = new_struct({'acc','fast'}, 'dim',[1,NUM_SESSION]);
 err_time = err_dir;
 err_both = err_dir;
 
+info = index_timing_errors_SAT(info, moves);
+
 for kk = 1:NUM_SESSION
   
   idx_acc = (info(kk).condition == 1);
@@ -29,9 +31,40 @@ end%for:sessions(kk)
 
 fprintf('Dir err: ACC %g +- %g  FAST %g +- %g\n', mean([err_dir.acc]), std([err_dir.acc])/sqrt(NUM_SESSION), ...
   mean([err_dir.fast]), std([err_dir.fast])/sqrt(NUM_SESSION))
-fprintf('Time err ACC %g +- %g  FAST %g +- %g\n', mean([err_time.acc]), std([err_time.acc])/sqrt(NUM_SESSION), ...
+fprintf('Time err ACC %g +- %g  FAST %g +- %g\n\n', mean([err_time.acc]), std([err_time.acc])/sqrt(NUM_SESSION), ...
   mean([err_time.fast]), std([err_time.fast])/sqrt(NUM_SESSION))
 % fprintf('Da dir + time err Acc/Fast: %g and %g\n', mean([err_both_Da.acc]), mean([err_both_Da.fast]))
+
+%% Split choice error rate by timing errors
+
+errdir_corr_A = NaN(1,NUM_SESSION);
+errdir_corr_F = NaN(1,NUM_SESSION);
+errdir_errtime_A = NaN(1,NUM_SESSION);
+errdir_errtime_F = NaN(1,NUM_SESSION);
+
+for kk = 1:NUM_SESSION
+
+  idx_acc = (info(kk).condition == 1);
+  idx_fast = (info(kk).condition == 3);
+  
+  ierr_dir = info(kk).err_dir;
+  ierr_time = info(kk).err_time;
+
+  errdir_corr_A(kk) = sum(ierr_dir & ~ierr_time & idx_acc) / sum(~ierr_time & idx_acc);
+  errdir_corr_F(kk) = sum(ierr_dir & ~ierr_time & idx_fast) / sum(~ierr_time & idx_fast);
+
+  errdir_errtime_A(kk) = sum(ierr_dir & ierr_time & idx_acc) / sum(ierr_time & idx_acc);
+  errdir_errtime_F(kk) = sum(ierr_dir & ierr_time & idx_fast) / sum(ierr_time & idx_fast);
+
+end%for:session(kk)
+
+fprintf('Choice errors with timing correct -- ACC %g +- %g  Fast %g +- %g\n', ...
+	mean(errdir_corr_A), std(errdir_corr_A)/sqrt(NUM_SESSION), ...
+	mean(errdir_corr_F), std(errdir_corr_F)/sqrt(NUM_SESSION))
+
+fprintf('Choice errors with timing error -- ACC %g +- %g  Fast %g +- %g\n', ...
+	mean(errdir_errtime_A), std(errdir_errtime_A)/sqrt(NUM_SESSION), ...
+	mean(errdir_errtime_F), std(errdir_errtime_F)/sqrt(NUM_SESSION))
 
 
 end%function:compute_avg_err_pcent_SAT()
---
+++ Vigor/compute_avg_vigor_SAT.m
@@ -0,0 +1,67 @@
+function [  ] = compute_avg_vigor_SAT( info , moves )
+%compute_avg_vigor_SAT Summary of this function goes here
+%   Detailed explanation goes here
+
+NUM_SESSION = length(moves);
+
+vigor_A = NaN(1,NUM_SESSION);
+vigor_F = NaN(1,NUM_SESSION);
+
+info = index_timing_errors_SAT(info, moves);
+
+for kk = 1:NUM_SESSION
+  
+  idx_acc = (info(kk).condition == 1);
+  idx_fast = (info(kk).condition == 3);
+  
+  ierr_dir = info(kk).err_dir;
+  ierr_time = info(kk).err_time;
+  
+  vigor_A(kk) = nanmean(moves(kk).vigor(idx_acc));
+  vigor_F(kk) = nanmean(moves(kk).vigor(idx_fast));
+
+  %err_dir(kk).acc = sum(idx_acc & ierr_dir) / sum(idx_acc);
+  %err_dir(kk).fast = sum(idx_fast & ierr_dir) / sum(idx_fast);
+  
+  %err_time(kk).acc = sum(idx_acc & ierr_time) / sum(idx_acc);
+  %err_time(kk).fast = sum(idx_fast & ierr_time) / sum(idx_fast);
+  
+end%for:sessions(kk)
+
+fprintf('Vigor: ACC %g +- %g  FAST %g +- %g\n', ...
+	nanmean(vigor_A), nanstd(vigor_A)/sqrt(NUM_SESSION), ...
+	nanmean(vigor_F), nanstd(vigor_F)/sqrt(NUM_SESSION))
+
+
+%% Now compute vigor X condition X choice error
+
+vigor_corr_A = NaN(1,NUM_SESSION);
+vigor_corr_F = NaN(1,NUM_SESSION);
+vigor_errdir_A = NaN(1,NUM_SESSION);
+vigor_errdir_F = NaN(1,NUM_SESSION);
+
+for kk = 1:NUM_SESSION
+
+  idx_acc = (info(kk).condition == 1);
+  idx_fast = (info(kk).condition == 3);
+  
+  idx_corr = ~(info(kk).err_dir | info(kk).err_hold);
+  idx_errdir = info(kk).err_dir;
+
+  vigor_corr_A(kk) = nanmean(moves(kk).vigor(idx_corr & idx_acc));
+  vigor_corr_F(kk) = nanmean(moves(kk).vigor(idx_corr & idx_fast));
+
+  vigor_errdir_A(kk) = nanmean(moves(kk).vigor(idx_errdir & idx_acc));
+  vigor_errdir_F(kk) = nanmean(moves(kk).vigor(idx_errdir & idx_fast));
+
+end%for:session(kk)
+
+fprintf('\nVigor -- Correct: ACC %g +- %g  FAST %g +- %g\n', ...
+	nanmean(vigor_corr_A), nanstd(vigor_corr_A)/sqrt(NUM_SESSION), ...
+	nanmean(vigor_corr_F), nanstd(vigor_corr_F)/sqrt(NUM_SESSION))
+
+fprintf('Vigor -- Choice Error: ACC %g +- %g  FAST %g +- %g\n', ...
+	nanmean(vigor_errdir_A), nanstd(vigor_errdir_A)/sqrt(NUM_SESSION), ...
+	nanmean(vigor_errdir_F), nanstd(vigor_errdir_F)/sqrt(NUM_SESSION))
+
+end%function:compute_avg_vigor_SAT()
