500f713552e99a804ec33466bda048c6b7d38973
treppert <thomas.reppert@gmail.com>
2018-08-07 14:55:17 -0500

    Added fxn plot_saccade_raster_SAT() to assess timing of saccades to fixation point. Also modified fxns calc_avg_baseline_SAT() and plot_baseline_vs_trial() to provide estimates of raw spike counts instead of avg values of the SDF at baseline.

--- Baseline/calc_avg_baseline_SAT.m
+++ Baseline/calc_avg_baseline_SAT.m
@@ -1,65 +1,136 @@
-function [ avg_bline , varargout ] = calc_avg_baseline_SAT( spikes , ninfo , binfo )
+function [ varargout ] = calc_avg_baseline_SAT( spikes , ninfo , binfo , moves )
 %calc_avg_baseline_SAT Summary of this function goes here
 %   Detailed explanation goes here
 
 TIME_STIM = 3500;
-TIME_BASE = ( -500 : -1 );
+TIME_BASE = ( -700 : -1 );
+IDX_BASE = TIME_BASE([1,end]) + TIME_STIM;
 
-MIN_GRADE = 3;
 NUM_CELLS = length(spikes);
 
-avg_bline = NaN(1,NUM_CELLS);
-sd_bline = NaN(1,NUM_CELLS);
+%initialize baseline X condition
+mu_bline = new_struct({'acc','fast','all'}, 'dim',[1,NUM_CELLS]);
+mu_bline = populate_struct(mu_bline, {'acc','fast','all'}, NaN);
+sd_bline = mu_bline;
 
-bline_Acc = NaN(1,NUM_CELLS);
-bline_Fast = NaN(1,NUM_CELLS);
-sd_Acc = NaN(1,NUM_CELLS);
-sd_Fast = NaN(1,NUM_CELLS);
+%initialize baseline X condition X error
+mu_bline_A = new_struct({'corr','err'}, 'dim',[1,NUM_CELLS]); %timing error
+mu_bline_A = populate_struct(mu_bline_A, {'corr','err'}, NaN);
+mu_bline_F = mu_bline_A; %direction error
 
-for kk = 1:NUM_CELLS
-%   if (ninfo(kk).vis < MIN_GRADE); continue; end
+binfo = index_timing_errors_SAT(binfo, moves);
+
+for cc = 1:NUM_CELLS
+  kk = ismember({binfo.session}, ninfo(cc).sesh);
   
-  sdf_kk = compute_spike_density_fxn( spikes(kk).SAT );
-  tmp = mean(sdf_kk(:,TIME_STIM + TIME_BASE), 2); %average per trial
+  idx_A = (binfo(kk).condition == 1);
+  idx_F = (binfo(kk).condition == 3);
   
-  avg_bline(kk) = mean(tmp);
-  sd_bline(kk) = std(tmp);
+  idx_corr = ~(binfo(kk).err_dir | binfo(kk).err_time | binfo(kk).err_hold);
+  idx_errdir = (binfo(kk).err_dir & ~binfo(kk).err_time);
+  idx_errtime = (~binfo(kk).err_dir & binfo(kk).err_time);
   
-  %% Now split on condition
+  num_sp_bline = NaN(1,binfo(kk).num_trials);
+  for jj = 1:binfo(kk).num_trials
+    num_sp_bline(jj) = sum((spikes(cc).SAT{jj} > IDX_BASE(1)) & (spikes(cc).SAT{jj} > IDX_BASE(2)));
+  end
   
-  kk_info = ismember({binfo.session}, ninfo(kk).sesh);
+  mu_bline(cc).all = mean(num_sp_bline);          sd_bline(cc).all = std(num_sp_bline);
+  mu_bline(cc).acc = mean(num_sp_bline(idx_A));   sd_bline(cc).acc = std(num_sp_bline(idx_A));
+  mu_bline(cc).fast = mean(num_sp_bline(idx_F));  sd_bline(cc).fast = std(num_sp_bline(idx_F));
   
-  idx_Acc = (binfo(kk_info).condition == 1);
-  idx_Fast = (binfo(kk_info).condition == 3);
-  
-  avg_acc = mean(sdf_kk(idx_Acc,TIME_STIM + TIME_BASE), 2);
-  avg_fast = mean(sdf_kk(idx_Fast,TIME_STIM + TIME_BASE), 2);
-  
-  bline_Acc(kk) = mean(avg_acc);
-  bline_Fast(kk) = mean(avg_fast);
-  
-  sd_Acc(kk) = std(avg_acc);
-  sd_Fast(kk) = std(avg_fast);
+  mu_bline_A(cc).corr = mean(num_sp_bline(idx_A & idx_corr));
+  mu_bline_A(cc).err = mean(num_sp_bline(idx_A & idx_errtime));
+  mu_bline_F(cc).corr = mean(num_sp_bline(idx_F & idx_corr));
+  mu_bline_F(cc).err = mean(num_sp_bline(idx_F & idx_errdir));
   
 end%for:cells(kk)
 
-if (nargout > 1)
-  varargout{1} = sd_bline;
+if (nargout > 0)
+  varargout{1} = mu_bline;
+  if (nargout > 1)
+    varargout{2} = sd_bline;
+  end
+  return
 end
 
-%% Plotting
+%% Plotting - Bar
+
+Y_BAR = [mean([mu_bline.acc]), mean([mu_bline_A.corr]), mean([mu_bline_A.err]), ...
+  mean([mu_bline.fast]), mean([mu_bline_F.corr]), mean([mu_bline_F.err])];
+
+Y_ERR = [std([mu_bline.acc]), std([mu_bline_A.corr]), std([mu_bline_A.err]), ...
+  std([mu_bline.fast]), std([mu_bline_F.corr]), std([mu_bline_F.err])] / sqrt(NUM_CELLS);
 
 figure(); hold on
-plot([0 70], [0 70], 'k--')
-errorbarxy(bline_Acc, bline_Fast, sd_Acc, sd_Fast, {'k.','k','k'})
-ppretty('image_size',[3,3])
+bar(1:6, Y_BAR, 0.5)
+errorbar_no_caps(1:6, Y_BAR, 'err',Y_ERR)
+ppretty()
 
-%bar plot
-err = [nanstd(bline_Acc) nanstd(bline_Fast)] / sqrt(sum([ninfo.vis] >= MIN_GRADE));
+[~,pval] = ttest([mu_bline_A.err] - [mu_bline_A.corr]);
+fprintf('T-test ACC (errtime - corr) -- pval = %g\n', pval)
+
+[~,pval] = ttest([mu_bline_F.err] - [mu_bline_F.corr]);
+fprintf('T-test FAST (errdir - corr) -- pval = %g\n', pval)
+
+pause(0.25)
+
+%% Plotting - CDF
+
+Y_CDF = (1:NUM_CELLS) / NUM_CELLS;
+
+X_cdf_A_corr = sort([mu_bline_A.corr]);
+X_cdf_A_err = sort([mu_bline_A.err]);
+X_cdf_F_corr = sort([mu_bline_F.corr]);
+X_cdf_F_err = sort([mu_bline_F.err]);
+
 figure(); hold on
-bar([1 2], [nanmean(bline_Acc) nanmean(bline_Fast)], 'BarWidth',0.4)
-errorbar_no_caps([1 2], [nanmean(bline_Acc) nanmean(bline_Fast)], 'err',err)
-xlim([0 3]); ppretty('image_size',[2,3])
+plot(X_cdf_A_corr, Y_CDF, 'r-', 'LineWidth',0.75)
+plot(X_cdf_A_err, Y_CDF, 'r--', 'LineWidth',1.5)
+ppretty()
 
-end%util:calc_avg_baseline_SAT()
+pause(0.25)
+
+figure(); hold on
+plot(X_cdf_F_corr, Y_CDF, '-', 'Color',[0 .7 0], 'LineWidth',0.75)
+plot(X_cdf_F_err, Y_CDF, '--', 'Color',[0 .7 0], 'LineWidth',1.5)
+ppretty()
+
+pause(0.25)
+
+%% Plotting - Histogram
+
+figure()
+subplot(3,1,1); histogram([mu_bline.all], 'FaceColor',[.6 .6 .6], 'BinWidth',10)
+subplot(3,1,2); histogram([mu_bline.acc], 'FaceColor','r', 'BinWidth',10)
+subplot(3,1,3); histogram([mu_bline.fast], 'FaceColor',[0 .7 0], 'BinWidth',10)
+ppretty('image_size',[6,8])
+
+pause(0.25)
+
+figure()
+subplot(3,1,1); histogram([sd_bline.all], 'FaceColor',[.6 .6 .6], 'BinWidth',5)
+subplot(3,1,2); histogram([sd_bline.acc], 'FaceColor','r', 'BinWidth',5)
+subplot(3,1,3); histogram([sd_bline.fast], 'FaceColor',[0 .7 0], 'BinWidth',5)
+ppretty('image_size',[6,8])
+
+pause(0.25)
+
+%% Plotting - Scatter
+
+figure(); hold on
+plot([mu_bline.acc], [mu_bline.fast], 'ko')
+plot([0 200], [0 200], '--', 'Color',[.5 .5 .5])
+ppretty('image_size',[5,4])
+
+pause(0.25)
+
+figure()
+histogram([mu_bline.fast] - [mu_bline.acc], 'FaceColor',[.5 .5 .5])
+ppretty()
+
+[~,pval] = ttest([mu_bline.fast] - [mu_bline.acc]);
+fprintf('T-test for sig. diff. (F - A) -- pval = %g\n', pval)
+
+end%fxn:calc_avg_baseline_SAT()
 
--- Baseline/plot_baseline_vs_trial.m
+++ Baseline/plot_baseline_vs_trial.m
@@ -4,62 +4,60 @@ function [  ] = plot_baseline_vs_trial( binfo , ninfo , spikes , bline_avg , mon
 
 NUM_CELLS = length(spikes);
 
-MIN_GRADE = 3;
-MIN_BLINE = 5; %sp/sec
-NUM_SEM = sum(([ninfo.vis] >= MIN_GRADE) & (bline_avg >= MIN_BLINE));
+MIN_BLINE = 2; %sp/sec
+NUM_SEM = sum([bline_avg.all] >= MIN_BLINE);
 
 TIME_STIM = 3500;
-TIME_BASE = ( -500 : -1 );
+TIME_BASE = ( -700 : -1 );
+IDX_BASE = TIME_BASE([1,end]) + TIME_STIM;
 
-TRIAL_PLOT = ( -2 : 1 ) ;
+TRIAL_PLOT = ( -3 : 2 ) ;
 NUM_TRIALS = length(TRIAL_PLOT);
 
 trial_switch = identify_condition_switch( binfo , monkey );
-% bline_avg = calc_avg_baseline_SAT( spikes );
 
 %% Compute baseline activity vs. trial
 
 bline_F2A = NaN(NUM_CELLS,NUM_TRIALS);
 bline_A2F = NaN(NUM_CELLS,NUM_TRIALS);
 
-for kk = 1:NUM_CELLS
-  if (ninfo(kk).vis < MIN_GRADE); continue; end
-  if (bline_avg(kk) < MIN_BLINE); continue; end
+for cc = 1:NUM_CELLS
+  if (bline_avg(cc).all < MIN_BLINE); continue; end
   
-  sesh = find(ismember({binfo.session}, ninfo(kk).sesh));
+  kk = find(ismember({binfo.session}, ninfo(cc).sesh));
+  
+  %count spikes in the appropriate baseline interval
+  num_sp_bline = NaN(1,binfo(kk).num_trials);
+  for jj = 1:binfo(kk).num_trials
+    num_sp_bline(jj) = sum((spikes(cc).SAT{jj} > IDX_BASE(1)) & (spikes(cc).SAT{jj} > IDX_BASE(2)));
+  end
   
   for jj = 1:NUM_TRIALS
-    
-    sdf_bline_F2A = compute_spike_density_fxn( spikes(kk).SAT(trial_switch(sesh).F2A + TRIAL_PLOT(jj)));
-    sdf_bline_A2F = compute_spike_density_fxn( spikes(kk).SAT(trial_switch(sesh).A2F + TRIAL_PLOT(jj)));
-    
-    bline_F2A(kk,jj) = mean(mean(sdf_bline_F2A(:,TIME_STIM + TIME_BASE)));
-    bline_A2F(kk,jj) = mean(mean(sdf_bline_A2F(:,TIME_STIM + TIME_BASE)));
-    
+    bline_F2A(cc,jj) = mean(num_sp_bline(trial_switch(kk).F2A + TRIAL_PLOT(jj)));
+    bline_A2F(cc,jj) = mean(num_sp_bline(trial_switch(kk).A2F + TRIAL_PLOT(jj)));
   end%for:trials(jj)
   
-end%for:cells(kk)
+end%for:cells(cc)
 
 %normalization
-bline_F2A = bline_F2A ./ bline_avg';
-bline_A2F = bline_A2F ./ bline_avg';
+bline_F2A = bline_F2A - repmat([bline_avg.all]', 1,NUM_TRIALS);
+bline_A2F = bline_A2F - repmat([bline_avg.all]', 1,NUM_TRIALS);
 
 %% Plotting
 
 figure(); hold on
+plot(TRIAL_PLOT, bline_F2A, 'k-', 'LineWidth',0.75)
+plot(TRIAL_PLOT, mean(bline_F2A), 'b-', 'LineWidth',1.5)
+% errorbar_no_caps(TRIAL_PLOT, nanmean(bline_F2A), 'err',nanstd(bline_F2A)/sqrt(NUM_SEM), 'color','k')
+ppretty()
 
-% plot([-2.5 -2.5], [0.8 1.2], 'k--')
-% plot([1.5 1.5], [0.8 1.2], 'k--')
+pause(0.25)
 
-% plot((-4:-1), bline_F2A, 'LineWidth',1.25, 'Color',[.4 .4 .4])
-% plot( (0:3),  bline_A2F, 'LineWidth',1.25, 'Color',[.4 .4 .4])
-errorbar_no_caps((-4:-1), nanmean(bline_F2A), 'err',nanstd(bline_F2A)/sqrt(NUM_SEM), 'color','k')
-errorbar_no_caps( (0:3),  nanmean(bline_A2F), 'err',nanstd(bline_A2F)/sqrt(NUM_SEM), 'color','k')
-
-xlim([-4.2 3.2]); xticks(-4 : 3)
-xticklabels({'-2','-1','0','+1','-2','-1','0','+1'})
-
-ppretty()%'image_size',[5,5])
+figure(); hold on
+plot(TRIAL_PLOT, bline_A2F, 'k-', 'LineWidth',0.75)
+plot(TRIAL_PLOT, mean(bline_A2F), 'b-', 'LineWidth',1.5)
+% errorbar_no_caps(TRIAL_PLOT,  nanmean(bline_A2F), 'err',nanstd(bline_A2F)/sqrt(NUM_SEM), 'color','k')
+ppretty()
 
 end%function:plot_baseline_vs_trial()
 
