e88610fa1cb388dd943dcf18a74b8c08f17b924b
treppert <thomas.reppert@gmail.com>
2018-08-21 12:14:19 -0500

    Modified fxn compute_avg_RT_SAT() to output bar plots of RT X condition X trial outcome (choice error vs. correct). Also modified fxn plot_baseline_vs_RT_SAT() to plot baseline spike counts relative to all trials (both correct and incorrect, Fast and Accurate). It also now plots line of best fit to the spike count vs. RT data.

--- Baseline/plot_baseline_vs_RT_SAT.m
+++ Baseline/plot_baseline_vs_RT_SAT.m
@@ -1,12 +1,18 @@
-function [ varargout ] = plot_baseline_vs_RT_SAT( binfo , moves , ninfo , spikes )
+function [ varargout ] = plot_baseline_vs_RT_SAT( binfo , moves , ninfo , spikes , condition )
 %plot_baseline_vs_RT_SAT Summary of this function goes here
 %   Detailed explanation goes here
 
 NORMALIZE = true;
 MIN_NUM_CELLS = 10; %for purposes of plotting across all cells
 
-BIN_RT = (300 : 40 : 700);
-% BIN_RT = (200 : 20 : 400);
+if strcmp(condition, 'acc')
+  BIN_RT = (400 : 40 : 700);
+elseif strcmp(condition, 'fast')
+  BIN_RT = (200 : 30 : 400);
+else
+  error('Input "condition" is incorrect')
+end
+
 RT_PLOT = BIN_RT(1:end-1) + diff(BIN_RT)/2;
 NUM_BIN = length(RT_PLOT);
 MIN_PER_BIN = 10; %minimum number of trials per RT bin
@@ -20,12 +26,12 @@ NUM_CELLS = length(spikes);
 binfo = index_timing_errors_SAT(binfo, moves);
 
 %initializations
-sp_A_Corr = NaN(NUM_CELLS,NUM_BIN);
-sd_A_Corr = NaN(NUM_CELLS,NUM_BIN);
+sp_Corr = NaN(NUM_CELLS,NUM_BIN);
+sd_Corr = NaN(NUM_CELLS,NUM_BIN);
 
 %stats
-rho_A = NaN(1,NUM_CELLS);
-pval_A = NaN(1,NUM_CELLS);
+rho = NaN(1,NUM_CELLS);
+pval = NaN(1,NUM_CELLS);
 
 for cc = 1:NUM_CELLS
   kk = ismember({binfo.session}, ninfo(cc).sesh);
@@ -33,8 +39,11 @@ for cc = 1:NUM_CELLS
   idx_A = (binfo(kk).condition == 1);
   idx_F = (binfo(kk).condition == 3);
   
-  idx_corr_F = (~(binfo(kk).err_dir | binfo(kk).err_time | binfo(kk).err_hold) & idx_F);
-  idx_corr_A = (~(binfo(kk).err_dir | binfo(kk).err_time | binfo(kk).err_hold) & idx_A);
+  if strcmp(condition, 'fast')
+    idx_corr = (~(binfo(kk).err_dir | binfo(kk).err_time | binfo(kk).err_hold) & idx_F);
+  else
+    idx_corr = (~(binfo(kk).err_dir | binfo(kk).err_time | binfo(kk).err_hold) & idx_A);
+  end
   
   RT = moves(kk).resptime;
   
@@ -51,19 +60,19 @@ for cc = 1:NUM_CELLS
 %   ppretty('image_size',[4.8,6])
   
   %use regression to determine cells that show a postive relationship
-  [rho_A(cc),pval_A(cc)] = corr(RT(idx_corr_A)', num_sp_bline(idx_corr_A)', 'type','Pearson');
-  if ~((rho_A(cc) > 0) && (pval_A(cc) < .05)); continue; end
+  [rho(cc),pval(cc)] = corr(RT(idx_corr)', num_sp_bline(idx_corr)', 'type','Pearson');
+  if ~((rho(cc) > 0) && (pval(cc) < .05)); continue; end
   
   %bin spike counts by RT
   for jj = 1:NUM_BIN
     idx_jj = ((RT > BIN_RT(jj)) & (RT < BIN_RT(jj+1)));
     
     if (sum(idx_jj) >= MIN_PER_BIN) %enforce min number of trials
-      sp_A_Corr(cc,jj) = mean(num_sp_bline(idx_corr_A & idx_jj));
-      sd_A_Corr(cc,jj) = std(num_sp_bline(idx_corr_A & idx_jj));
+      sp_Corr(cc,jj) = mean(num_sp_bline(idx_corr & idx_jj));
+      sd_Corr(cc,jj) = std(num_sp_bline(idx_corr & idx_jj));
       
       if (NORMALIZE)
-        sp_A_Corr(cc,jj) = sp_A_Corr(cc,jj) - mean(num_sp_bline(idx_corr_A | idx_corr_F));
+        sp_Corr(cc,jj) = sp_Corr(cc,jj) - mean(num_sp_bline(idx_A | idx_F));
       end
       
     end
@@ -77,20 +86,34 @@ for cc = 1:NUM_CELLS
 end%for:cells(cc)
 
 if (nargout > 0)
-  varargout{1} = rho_A;
+  varargout{1} = rho;
   if (nargout > 1)
-    varargout{2} = pval_A;
+    varargout{2} = pval;
   end
 end
 
 %% Plotting
-sp_A_Corr(:,sum(~isnan(sp_A_Corr),1) < MIN_NUM_CELLS) = NaN;
-NUM_SEM = sum(~isnan(sp_A_Corr),1);
+if strcmp(condition, 'acc')
+  COLOR_PLOT = 'r';
+else
+  COLOR_PLOT = [0 .7 0];
+end
+
+sp_Corr(:,sum(~isnan(sp_Corr),1) < MIN_NUM_CELLS) = NaN;
+NUM_SEM = sum(~isnan(sp_Corr),1);
+
+%perform a linear fit to the data
+xx_fit = repmat(RT_PLOT', NUM_CELLS,1);
+yy_fit = reshape(sp_Corr', NUM_CELLS*NUM_BIN,1);
+i_nan = isnan(yy_fit);
+xx_fit(i_nan) = [];
+yy_fit(i_nan) = [];
+[fit_lin,gof_lin] = fit(xx_fit, yy_fit, 'poly1');
 
 figure(); hold on
-plot([450 650], [0 0], 'k--')
-% plot([250 350], [0 0], 'k--')
-errorbar_no_caps(RT_PLOT, nanmean(sp_A_Corr), 'err',nanstd(sp_A_Corr)./sqrt(NUM_SEM), 'color','r')
+% plot(RT_PLOT, sp_Corr, 'ko')
+plot(RT_PLOT, fit_lin(RT_PLOT), '-', 'color',COLOR_PLOT)
+errorbar_no_caps(RT_PLOT, nanmean(sp_Corr), 'err',nanstd(sp_Corr)./sqrt(NUM_SEM), 'color',COLOR_PLOT)
 ppretty()
 
 pause(0.25)
@@ -99,7 +122,7 @@ figure(); hold on
 plot([-.2 .3], -log(.05)*ones(1,2), '-', 'Color',[.5 .5 .5])
 plot([-.2 .3], -log(.01)*ones(1,2), '-', 'Color',[.5 .5 .5])
 plot([-.2 .3], -log(.001)*ones(1,2), '-', 'Color',[.5 .5 .5])
-plot(rho_A, -log(pval_A), 'ko')
+plot(rho, -log(pval), 'ko')
 ppretty('image_size',[3.2,5])
 
 end%fxn:plot_baseline_vs_RT_SAT()
--- RespTime/compute_avg_RT_SAT.m
+++ RespTime/compute_avg_RT_SAT.m
@@ -6,6 +6,9 @@ NUM_SESSIONS = length(moves);
 
 RT = new_struct({'acc','fast'}, 'dim',[1,NUM_SESSIONS]);
 
+RTacc = new_struct({'corr','errdir'}, 'dim',[1,NUM_SESSIONS]); %save RT X condition X choice error
+RTfast = new_struct({'corr','errdir'}, 'dim',[1,NUM_SESSIONS]);
+
 for kk = 1:NUM_SESSIONS
   
   idx_acc = (info(kk).condition == 1);
@@ -13,6 +16,14 @@ for kk = 1:NUM_SESSIONS
   
   RT(kk).acc = nanmean(moves(kk).resptime(idx_acc));
   RT(kk).fast = nanmean(moves(kk).resptime(idx_fast));
+  
+  idx_corr = ~(info(kk).err_dir | info(kk).err_hold | info(kk).err_nosacc);
+  idx_errdir = info(kk).err_dir;
+  
+  RTacc(kk).corr = nanmean(moves(kk).resptime(idx_acc & idx_corr));
+  RTacc(kk).errdir = nanmean(moves(kk).resptime(idx_acc & idx_errdir));
+  RTfast(kk).corr = nanmean(moves(kk).resptime(idx_fast & idx_corr));
+  RTfast(kk).errdir = nanmean(moves(kk).resptime(idx_fast & idx_errdir));
   
 end%for:sessions(kk)
 
@@ -23,8 +34,8 @@ fprintf('RT Fast: %g +/- %g ms\n', mean([RT.fast]), std([RT.fast])/sqrt(NUM_SESS
 [~,pval,~,tstat] = ttest([RT.acc], [RT.fast], 'tail','both');
 fprintf('pval = %g || t(%d) = %g\n', pval, tstat.df, tstat.tstat)
 
-%% Plotting
-
+%% Plotting -- RT X condition
+if (false)
 figure(); hold on
 
 mu = [ mean([RT.acc]) , mean([RT.fast]) ];
@@ -34,6 +45,23 @@ errorbar_no_caps([.95 1.95], mu, 'err',err)
 
 xticks([]); xlim([.90 2.10])
 ppretty('image_size',[1.2,3])
+end
+%% Plotting -- RT X condition X error
+
+figure(); hold on
+
+mu_A = [ mean([RTacc.corr]) , mean([RTacc.errdir]) ];
+err_A = [ std([RTacc.corr]) , std([RTacc.errdir]) ] / sqrt(NUM_SESSIONS);
+mu_F = [ mean([RTfast.corr]) , mean([RTfast.errdir]) ];
+err_F = [ std([RTfast.corr]) , std([RTfast.errdir]) ] / sqrt(NUM_SESSIONS);
+
+bar([.95 1.95], mu_A, 0.5)
+bar([2.95 3.95], mu_F, 0.5)
+errorbar_no_caps([.95 1.95], mu_A, 'err',err_A)
+errorbar_no_caps([2.95 3.95], mu_F, 'err',err_F)
+
+xticks([]); xlim([.5 4.5])
+ppretty('image_size',[2.2,3])
 
 end%function:compute_avg_RT_SAT()
 
