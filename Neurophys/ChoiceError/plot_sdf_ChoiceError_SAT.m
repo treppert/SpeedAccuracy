0ec9bd34f2df6e45c1c046c8ea64bd16cb523578
Thomas Reppert <thomas.reppert@gmail.com>
2018-10-22 15:12:04 -0500

    Finished edits to neurophys choice error plots such that we now take into account the timing of the corrective saccade. All SDFs are clipped at the time of corrective saccade.

--- Neurophys/ChoiceError/plot_sdf_ChoiceError_SAT.m
+++ Neurophys/ChoiceError/plot_sdf_ChoiceError_SAT.m
@@ -2,13 +2,13 @@ function [ varargout ] = plot_sdf_ChoiceError_SAT( spikes , ninfo , moves , move
 %plot_baseline_activity Summary of this function goes here
 %   Detailed explanation goes here
 
-PLOT_INDIVIDUAL_CELLS = false;
-NORMALIZE = true;
+CLIP_SDF_POST_CORRECTIVE = true;
+PLOT_INDIVIDUAL_CELLS = true;
 
 TIME_POSTSACC  = 3500 + (-400 : 400);
 NSAMP_POSTSACC = length(TIME_POSTSACC);
 TIME_TEST_MANNWHITNEY = 3500 + (1 : 400);
-NUM_CELLS = length(spikes);
+NUM_CELLS = 20;%length(spikes);
 
 %activity re. saccade initiation
 A_corr = NaN(NUM_CELLS,NSAMP_POSTSACC);
@@ -52,30 +52,31 @@ for cc = 1:NUM_CELLS
   [idx_err, idx_corr] = equate_respdir_err_vs_corr(idx_err, idx_corr, moves(kk).octant);
   
   %remove any activity related to corrective saccade initiation
-  sdf_kk(idx_cond & idx_err,:) = rem_spikes_post_corrective_SAT(sdf_kk(idx_cond & idx_err,:), movesAll(kk));
+  if (CLIP_SDF_POST_CORRECTIVE)
+    trial_err = find(idx_cond & idx_err);
+    sdf_kk(idx_cond & idx_err,:) = rem_spikes_post_corrective_SAT(sdf_kk(idx_cond & idx_err,:), movesAll(kk), trial_err);
+  end
   
   A_corr(cc,:) = nanmean(sdf_kk(idx_cond & idx_corr,TIME_POSTSACC));
   A_err(cc,:) = nanmean(sdf_kk(idx_cond & idx_err,TIME_POSTSACC));
   
   %assess time of separation between A_corr and A_err
-  sdf_corr_test = sdf_kk(idx_cond & idx_corr,TIME_TEST_MANNWHITNEY);
-  sdf_err_test = sdf_kk(idx_cond & idx_err,TIME_TEST_MANNWHITNEY);
-  t_sep_err(cc) = compute_time_sep_sdf_SAT(sdf_corr_test, sdf_err_test, 'min_length',20);
+%   sdf_corr_test = sdf_kk(idx_cond & idx_corr,TIME_TEST_MANNWHITNEY);
+%   sdf_err_test = sdf_kk(idx_cond & idx_err,TIME_TEST_MANNWHITNEY);
+%   t_sep_err(cc) = compute_time_sep_sdf_SAT(sdf_corr_test, sdf_err_test, 'min_length',20);
   
   %assess direction of error-related modulation (if any)
-  if isnan(t_sep_err(cc))
-    dir_sep_err{cc} = 'N';
-  elseif (A_err(cc,400+t_sep_err(cc)) > A_corr(cc,400+t_sep_err(cc)))
-    dir_sep_err{cc} = 'E';
-  else
-    dir_sep_err{cc} = 'C';
-  end
+%   if isnan(t_sep_err(cc))
+%     dir_sep_err{cc} = 'N';
+%   elseif (A_err(cc,400+t_sep_err(cc)) > A_corr(cc,400+t_sep_err(cc)))
+%     dir_sep_err{cc} = 'E';
+%   else
+%     dir_sep_err{cc} = 'C';
+%   end
   
   %save median RTs
   RT_corr(cc) = nanmedian(moves(kk).resptime(idx_cond & idx_corr));
   RT_err(cc) = nanmedian(moves(kk).resptime(idx_cond & idx_err));
-  
-%   pause(1.0)
   
 end%for:cells(cc)
 
@@ -86,33 +87,8 @@ if (nargout > 0)
   end
 end
 
-fprintf('%d and %d (out of %d) cells with E- and C-modulation\n', sum(ismember(dir_sep_err, {'E'})), ...
-  sum(ismember(dir_sep_err, {'C'})), NUM_CELLS)
-
-%% Plotting - across-cell average
-TIME_PLOT = TIME_POSTSACC - 3500;
-% IDX_CC_PLOT = ismember(dir_sep_err, {'E'});
-IDX_CC_PLOT = true(1,NUM_CELLS);
-
-A_DIFF = A_err(IDX_CC_PLOT,:) - A_corr(IDX_CC_PLOT,:);
-
-if (NORMALIZE)
-%   NORM_FACTOR = max(A_DIFF,[],2);
-  NORM_FACTOR = A_corr(:,400);
-  A_DIFF = A_DIFF ./ NORM_FACTOR;
-end
-
-figure(); hold on
-
-% plot(TIME_PLOT, A_DIFF, 'k-')
-shaded_error_bar(TIME_PLOT, mean(A_DIFF), std(A_DIFF)/sqrt(sum(IDX_CC_PLOT)), ...
-  {'LineWidth',1.5, 'Color',[0 .5 0]})
-
-xlim([TIME_PLOT(1), TIME_PLOT(end)])
-xlabel('Time re. saccade (ms)')
-ylabel('Difference in normalized activity')
-
-ppretty('image_size',[6,4])
+% fprintf('%d and %d (out of %d) cells with E- and C-modulation\n', sum(ismember(dir_sep_err, {'E'})), ...
+%   sum(ismember(dir_sep_err, {'C'})), NUM_CELLS)
 
 
 %% Plotting - individual cells
@@ -123,7 +99,7 @@ for cc = 1:NUM_CELLS
 %   if ~strcmp(dir_sep_err{cc}, 'C'); continue; end
   lim_lin = [min([A_corr(cc,:), A_err(cc,:)]), max([A_corr(cc,:), A_err(cc,:)])];
   
-  figure(); hold on
+  figure(cc); hold on
   
   plot([0 0], lim_lin, 'k--', 'LineWidth',1.0)
   plot(-RT_corr(cc)*ones(1,2), lim_lin, '-', 'Color',[0 .5 0])
@@ -138,12 +114,38 @@ for cc = 1:NUM_CELLS
   ylabel('Activity (sp/sec)')
   print_session_unit(gca, ninfo(cc))
   
-  ppretty('image_size',[6,4])
-  print_fig_SAT(ninfo(cc), gcf, '-dtiff')
+  ppretty('image_size',[5,3])
+%   print(['C:\Users\TDT\Dropbox/tmp/',ninfo(cc).sess,'-',ninfo(cc).unit,'.tif'], '-dtiff')
+%   print_fig_SAT(ninfo(cc), gcf, '-dtiff')
   
   pause(0.5)
   
 end%for:cells(cc)
 end%if(PLOT_INDIVIDUAL_CELLS)
+
+
+%% Plotting - across-cell average
+% TIME_PLOT = TIME_POSTSACC - 3500;
+% % IDX_CC_PLOT = ismember(dir_sep_err, {'E'});
+% IDX_CC_PLOT = true(1,NUM_CELLS);
+% 
+% A_DIFF = A_err(IDX_CC_PLOT,:) - A_corr(IDX_CC_PLOT,:);
+% 
+% %normalization of activity
+% %   NORM_FACTOR = max(A_DIFF,[],2);
+% NORM_FACTOR = A_corr(:,400);
+% A_DIFF = A_DIFF ./ NORM_FACTOR;
+% 
+% figure(); hold on
+% 
+% shaded_error_bar(TIME_PLOT, mean(A_DIFF), std(A_DIFF)/sqrt(sum(IDX_CC_PLOT)), ...
+%   {'LineWidth',1.5, 'Color',[0 .5 0]})
+% 
+% xlim([TIME_PLOT(1), TIME_PLOT(end)])
+% xlabel('Time re. saccade (ms)')
+% ylabel('Difference in normalized activity')
+% 
+% ppretty('image_size',[6,4])
+
 
 end%function:plot_sdf_error_SEF()
--- Neurophys/ChoiceError/rem_spikes_post_corrective_SAT.m
+++ Neurophys/ChoiceError/rem_spikes_post_corrective_SAT.m
@@ -1,9 +1,9 @@
-function [ sdf_out ] = rem_spikes_post_corrective_SAT( sdf_in , movesAll )
-%[ sig_out ] = remove_spikes_post_response( sig_in , movesAll.resptime )
+function [ sdf_out ] = rem_spikes_post_corrective_SAT( sdf_in , movesAll , trial_err )
+%[ sig_out ] = rem_spikes_post_corrective_SAT( sig_in , movesAll.resptime )
 %   Detailed explanation goes here
 
 LEAD_TIME = 0;
-TIME_PRIMARY_RESP = 3500; %note -- SDF has already been centered at primary resp.
+TIME_ZERO = 3500; %note -- SDF has already been centered at primary resp.
 
 [NUM_TRIAL, NUM_SAMP] = size(sdf_in);
 
@@ -13,33 +13,25 @@ sdf_out = sdf_in;
 %get timing of corrective saccade
 RT_corrective = NaN(1,NUM_TRIAL);
 for jj = 1:NUM_TRIAL
+  idx_jj = find(movesAll.trial == trial_err(jj));
   
-  idx_jj = (movesAll.trial == jj);
-  
-  %NOTE -- This needs to be indexed correctly -- i.e. pass idx_err as
-  %input!!
-  if (sum(idx_jj) < 2)
-    error('No corrective saccade on Trial %d', jj)
-  end
-  
+  if (length(idx_jj) < 2); continue; end
+  RT_corrective(jj) = double(movesAll.resptime(idx_jj(2)) - movesAll.resptime(idx_jj(1)));
 end%for:trials(jj)
 
-%loop over all trials and remove response-locked signal
+%remove post-corrective saccade spikes (SDF already aligned on RT)
 for jj = 1:NUM_TRIAL
+  if isnan(RT_corrective(jj)); continue; end
   
-  %if response time is zero or NaN, just continue
-  if isnan(movesAll.resptime(jj)); continue; end
-  
-  idx_remove = TIME_PRIMARY_RESP + movesAll.resptime(jj) - LEAD_TIME;
-  
-  %remove spikes post-response
-  sdf_out(jj,idx_remove:NUM_SAMP) = NaN;
-  
+  idx_rem = TIME_ZERO + RT_corrective(jj) - LEAD_TIME;
+  sdf_out(jj,idx_rem:NUM_SAMP) = NaN;
 end%for:trials(jj)
 
-%remove signals from all trials after median RT
-idx_remove = TIME_PRIMARY_RESP + round(nanmedian(movesAll.resptime)) - LEAD_TIME;
+%take care of any trials with no corrective saccade
+sdf_out(isnan(RT_corrective),:) = NaN;
 
+%remove signals from all trials after median corrective saccade RT
+idx_remove = TIME_ZERO + round(nanmedian(RT_corrective)) - LEAD_TIME;
 sdf_out(:,idx_remove:NUM_SAMP) = NaN;
 
 end%utility:rem_spikes_post_corrective_SAT()
