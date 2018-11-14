6617a5fb5568cd214c74cd47dd28bce273a82a5b
treppert <thomas.reppert@gmail.com>
2018-11-14 17:02:49 -0600

    Modified fxns P-R/plot_distr_t_pp_scc() and P-R/plot_polar_distr_ppsacc_endpt() to only include post-primary saccades on choice error trials.

--- Post-Response/plot_distr_t_postprimary_sacc.m
+++ Post-Response/plot_distr_t_postprimary_sacc.m
@@ -1,48 +1,40 @@
-function [ varargout ] = plot_distr_t_postprimary_sacc( moves , movesAll , binfo )
+function [ ] = plot_distr_t_postprimary_sacc( moves , movesAll , binfo )
 %plot_distr_t_postprimary_sacc Summary of this function goes here
 %   Detailed explanation goes here
 
+INDEX = 2; %index of saccade post-primary
+% T_BIN_EDGES = (0 : 25 : 800);
 NUM_SESSION = length(moves);
-BIN_EDGES = (0 : 25 : 800);
 
-t_pp = [];
-t_pp_out = cell(1,NUM_SESSION);
+t_ppsacc = [];
 
 for kk = 1:NUM_SESSION
   
-  num_trial = length(moves(kk).clipped);
-  t_pp_out{kk} = NaN(1,num_trial);
-  
+  %index trials by condition and trial outcome
+  idx_fast = (binfo(kk).condition == 3);
   idx_errdir = (binfo(kk).err_dir & ~binfo(kk).err_time);
-  trial_errdir = find(idx_errdir);
+  trial_errdir = find(idx_fast & idx_errdir);
   
-  for jj = 1:num_trial
-    
-    if ~ismember(jj, trial_errdir); continue; end
-    
-    idx_jj = find(movesAll(kk).trial == jj);
-    
-    if (length(idx_jj) < 2); continue; end
-    
-    t_primary_jj = moves(kk).resptime(jj);
-    t_postprim_jj = double(movesAll(kk).resptime(idx_jj(2)) - t_primary_jj);
-    
-    if (t_postprim_jj > BIN_EDGES(end)); continue; end
-    
-    t_pp = cat(2, t_pp, t_postprim_jj);
-    t_pp_out{kk}(jj) = t_postprim_jj;
-    
-  end%for:trial(jj)
+  %index saccades by number from primary saccade
+  idxAll_ppsacc = (ismember(movesAll(kk).trial, trial_errdir) & (movesAll(kk).index == INDEX));
+  
+  %only take trials for which we have a post-primary saccade
+  trial_errdir = movesAll(kk).trial(idxAll_ppsacc);
+  
+  t_ppsacc_kk = double(movesAll(kk).resptime(idxAll_ppsacc) - moves(kk).resptime(trial_errdir));
+  t_ppsacc = cat(2, t_ppsacc, t_ppsacc_kk);
   
 end%for:session(kk)
 
-figure()
-histogram(t_pp, BIN_EDGES, 'Normalization','probability', 'FaceColor',[.2 .2 .2])
-ppretty()
+%% Plotting
 
-if (nargout > 0)
-  varargout{1} = t_pp_out;
-end
+t_plot = sort(t_ppsacc);
+y_plot = (1:length(t_plot)) / length(t_plot);
+
+figure()
+% histogram(t_ppsacc, T_BIN_EDGES, 'Normalization','probability', 'FaceColor',[.2 .2 .2])
+plot(t_plot, y_plot, 'g-', 'LineWidth',1.25)
+ppretty()
 
 end%fxn:plot_distr_t_postprimary_sacc()
 
--- Post-Response/plot_polar_distr_ppsacc_endpt.m
+++ Post-Response/plot_polar_distr_ppsacc_endpt.m
@@ -1,41 +1,49 @@
-function [] = plot_polar_distr_ppsacc_endpt( info , movesAll )
+function [] = plot_polar_distr_ppsacc_endpt( binfo , movesAll )
 %plot_polar_distr_ppsacc_endpt Summary of this function goes here
 %   Detailed explanation goes here
 
-NUM_SESSION = length(info);
+ROTATE_TGT_LOCATION = true;
+INDEX = 2; %index of saccade post-primary
+NUM_SESSION = length(binfo);
 
-octant_ppsacc = [];
+xfin_ppsacc = [];
+yfin_ppsacc = [];
 
 for kk = 1:NUM_SESSION
   
-  for jj = 1:info(kk).num_trials
+  %index trials by condition and trial outcome
+  idx_fast = (binfo(kk).condition == 1);
+  idx_errdir = (binfo(kk).err_dir & ~binfo(kk).err_time);
+  trial_errdir = find(idx_fast & idx_errdir);
+  
+  %index saccades by number from primary saccade
+  idxAll_ppsacc = (ismember(movesAll(kk).trial, trial_errdir) & (movesAll(kk).index == INDEX));
+  
+  %only take trials for which we have a post-primary saccade
+  trial_errdir = movesAll(kk).trial(idxAll_ppsacc);
+  
+  if (ROTATE_TGT_LOCATION)
+    %determine location of singleton relative to absolute right
+    th_tgt = convert_tgt_octant_to_angle(binfo(kk).tgt_octant(trial_errdir));
+    %rotate post-primary saccade trajectory according to singleton loc.
+    xtmp = cos(2*pi-th_tgt) .* movesAll(kk).x_fin(idxAll_ppsacc) - sin(2*pi-th_tgt) .* movesAll(kk).y_fin(idxAll_ppsacc);
+    ytmp = sin(2*pi-th_tgt) .* movesAll(kk).x_fin(idxAll_ppsacc) + cos(2*pi-th_tgt) .* movesAll(kk).y_fin(idxAll_ppsacc);
     
-    idx_jj = find(movesAll(kk).trial == jj);
-    
-    if (length(idx_jj) < 2); continue; end
-    
-    tgt_octant = uint16(info(kk).tgt_octant(jj));
-    ppsacc_oct_jj = movesAll(kk).octant(idx_jj(2)) - tgt_octant;
-    
-    octant_ppsacc = cat(2, octant_ppsacc, ppsacc_oct_jj);
-    
-  end%for:trial(jj)
+    xfin_ppsacc = cat(2, xfin_ppsacc, xtmp);
+    yfin_ppsacc = cat(2, yfin_ppsacc, ytmp);
+  else %no rotation -- absolute endpoint location
+    xfin_ppsacc = cat(2, xfin_ppsacc, movesAll(kk).x_fin(idxAll_ppsacc));
+    yfin_ppsacc = cat(2, yfin_ppsacc, movesAll(kk).y_fin(idxAll_ppsacc));
+  end
   
 end%for:session(kk)
 
-%keep convention that 1 = rightward (in this case, to the correct tgt)
-octant_ppsacc = octant_ppsacc + 1;
-
-%convert to appropriate format for polarscatter()
-weight_octant = NaN(1,8);
-for jj = 1:8
-  weight_octant(jj) = sum(octant_ppsacc == jj);
-end%for:octant(jj)
+TH_PPSACC = atan2(yfin_ppsacc, xfin_ppsacc);
+R_PPSACC = sqrt(xfin_ppsacc.*xfin_ppsacc + yfin_ppsacc.*yfin_ppsacc);
 
 figure(); polaraxes()
-polarscatter((0:pi/4:7*pi/4), 6*ones(1,8), weight_octant/4, [.2 .2 .2])
-rlim([0 8]); rticklabels([]); thetaticks([])
+polarscatter(TH_PPSACC, R_PPSACC, 10.0, [.2 .2 .2])
+rlim([0 10]); rticklabels([]); thetaticks([])
 ppretty()
 
 end%fxn:plot_polar_distr_ppsacc_endpt()
-
