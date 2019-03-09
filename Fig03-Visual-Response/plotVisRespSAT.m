4bb6c4c75cc8fe7453a3f3d7a91071b6d4bffaf4
treppert <thomas.reppert@gmail.com>
2019-03-09 12:39:14 -0600

    Updated fxn plotVisRespSAT() with sub-utility to plot individual cells. This cleans up the code quite a bit.

--- Fig03-Visual-Response/plotVisRespSAT.m
+++ Fig03-Visual-Response/plotVisRespSAT.m
@@ -4,37 +4,36 @@ function [  ] = plotVisRespSAT( binfo , moves , ninfo , spikes , varargin )
 
 args = getopt(varargin, {{'area=','SEF'}, {'monkey=',{'D','E'}}});
 
-idx_area = ismember({ninfo.area}, args.area);
-idx_monkey = ismember({ninfo.monkey}, args.monkey);
+idxArea = ismember({ninfo.area}, args.area);
+idxMonkey = ismember({ninfo.monkey}, args.monkey);
+%remove cells w/o correct form of visual response
+idxVis = ismember({ninfo.visType}, {'sustained'});
 
-ninfo = ninfo(idx_area & idx_monkey);
-spikes = spikes(idx_area & idx_monkey);
+ninfo = ninfo(idxArea & idxMonkey & idxVis);
+spikes = spikes(idxArea & idxMonkey & idxVis);
 
 NUM_CELLS = length(spikes);
 T_BASE = 3500 + (-600 : -1);
 T_STIM = 3500 + (-100 : 300);
-T_RESP_ACC = 3500 + (-300 : 100);
-T_RESP_FAST = 3500 + (-100 : 100);
+T_RESP.Acc = 3500 + (-300 : 100);
+T_RESP.Fast = 3500 + (-100 : 100);
 
-%initializations
-visRespAcc = NaN(NUM_CELLS,length(T_STIM));
-visRespFast = NaN(NUM_CELLS,length(T_STIM));
-sdfMoveAcc = NaN(NUM_CELLS,length(T_RESP_ACC));
-sdfMoveFast = NaN(NUM_CELLS,length(T_RESP_FAST));
-RTAcc = NaN(1,NUM_CELLS);
-RTFast = NaN(1,NUM_CELLS);
-meanBlineAcc = NaN(1,NUM_CELLS);
-meanBlineFast = NaN(1,NUM_CELLS);
-
-%visual response latency
+%visual response latency parameters
 MIN_DUR_VR = 50; %mininum duration of the visual response (ms)
 SD_THRESH_VR = [6, 2]; %minimum number of SDs from the mean (threshold)
-latVRAcc = NaN(1,NUM_CELLS);
-latVRFast = NaN(1,NUM_CELLS);
+
+%output initializations
+visResp = new_struct({'Acc','Fast'}, 'dim',[1,NUM_CELLS]);
+visResp = populate_struct(visResp, {'Acc','Fast'}, NaN(length(T_STIM),1));
+sdfMove = new_struct({'Acc','Fast'}, 'dim',[1,NUM_CELLS]);
+sdfMove = populate_struct(sdfMove, {'Acc'}, NaN(length(T_RESP.Acc),1));
+sdfMove = populate_struct(sdfMove, {'Fast'}, NaN(length(T_RESP.Fast),1));
+RT = new_struct({'Acc','Fast'}, 'dim',[1,NUM_CELLS]);
+RT = populate_struct(RT, {'Acc','Fast'}, NaN);
+meanBline = RT;
+latVR = RT;
 
 for cc = 1:NUM_CELLS
-  if ~strcmp(ninfo(cc).visType, 'sustained'); continue; end
-  
   kk = ismember({binfo.session}, ninfo(cc).sess);
   RTkk = double(moves(kk).resptime);
   
@@ -51,107 +50,97 @@ for cc = 1:NUM_CELLS
   %index by response dir re. response field
   idxRF = ismember(moves(kk).octant, ninfo(cc).visField);
   
-  visRespAcc(cc,:) = nanmean(sdfKKstim(idxAcc & idxCorr & idxRF, T_STIM));
-  visRespFast(cc,:) = nanmean(sdfKKstim(idxFast & idxCorr & idxRF, T_STIM));
-  sdfMoveAcc(cc,:) = nanmean(sdfKKresp(idxAcc & idxCorr & idxRF, T_RESP_ACC));
-  sdfMoveFast(cc,:) = nanmean(sdfKKresp(idxFast & idxCorr & idxRF, T_RESP_FAST));
-  
-  RTAcc(cc) = median(RTkk(idxAcc & idxCorr & idxRF));
-  RTFast(cc) = median(RTkk(idxFast & idxCorr & idxRF));
-  
+  %compute SDF
+  visResp(cc).Acc(:) = nanmean(sdfKKstim(idxAcc & idxCorr & idxRF, T_STIM));
+  visResp(cc).Fast(:) = nanmean(sdfKKstim(idxFast & idxCorr & idxRF, T_STIM));
+  sdfMove(cc).Acc(:) = nanmean(sdfKKresp(idxAcc & idxCorr & idxRF, T_RESP.Acc));
+  sdfMove(cc).Fast(:) = nanmean(sdfKKresp(idxFast & idxCorr & idxRF, T_RESP.Fast));
+  %compute median RT
+  RT(cc).Acc = median(RTkk(idxAcc & idxCorr & idxRF));
+  RT(cc).Fast = median(RTkk(idxFast & idxCorr & idxRF));
+  %compute baseline activity
   blineAcc = nanmean(sdfKKstim(idxAcc & idxCorr, T_BASE));
   blineFast = nanmean(sdfKKstim(idxFast & idxCorr, T_BASE));
-  meanBlineAcc(cc) = mean(blineAcc);
-  meanBlineFast(cc) = mean(blineFast);
+  meanBline(cc).Acc = mean(blineAcc);
+  meanBline(cc).Fast = mean(blineFast);
   
-  %% Compute visual response latency
-  latVRAcc(cc) = computeVisRespLatency(visRespAcc(cc,:), blineAcc, SD_THRESH_VR, MIN_DUR_VR, T_STIM);
-  latVRFast(cc) = computeVisRespLatency(visRespFast(cc,:), blineFast, SD_THRESH_VR, MIN_DUR_VR, T_STIM);
+  %compute visual response latency
+  latVR(cc).Acc = computeVisRespLatency(visResp(cc).Acc(:), blineAcc, SD_THRESH_VR, MIN_DUR_VR, T_STIM);
+  latVR(cc).Fast = computeVisRespLatency(visResp(cc).Fast(:), blineFast, SD_THRESH_VR, MIN_DUR_VR, T_STIM);
   
-  %% Plotting
-  figure()
-  
-  tmp = [visRespAcc(cc,:) visRespFast(cc,:) sdfMoveAcc(cc,:) sdfMoveFast(cc,:)];
-  yLim = [min(tmp) max(tmp)];
-  
-  %visual response
-  subplot(1,2,1); hold on
-  plot([0 0], yLim, 'k--')
-  plot(T_STIM-3500, visRespAcc(cc,:), 'r-', 'LineWidth',0.5)
-  plot(T_STIM-3500, visRespFast(cc,:), '-', 'Color',[0 .7 0], 'LineWidth',0.5)
-  plot(latVRAcc(cc)*ones(1,2), yLim, 'r:', 'LineWidth',0.5)
-  plot(latVRFast(cc)*ones(1,2), yLim, ':', 'Color',[0 .7 0], 'LineWidth',0.5)
-  plot([T_STIM(1) T_STIM(end)]-3500, meanBlineFast(cc)*ones(1,2), 'k:', 'LineWidth',0.5)
-%   plot([T_STIM(1) T_STIM(end)]-3500, THRESH_TOP*ones(1,2), 'k--', 'LineWidth',0.5)
-%   plot([T_STIM(1) T_STIM(end)]-3500, THRESH_BOTTOM*ones(1,2), 'k--', 'LineWidth',0.5)
-  plot(RTAcc(cc)*ones(1,2), yLim, 'r:', 'LineWidth',0.5)
-  plot(RTFast(cc)*ones(1,2), yLim, ':', 'Color',[0 .7 0], 'LineWidth',0.5)
-  xlim([T_STIM(1) T_STIM(end)]-3500)
-  print_session_unit(gca , ninfo(cc), binfo(kk))
-  
-  %activity from primary response
-  subplot(1,2,2); hold on
-  plot([0 0], yLim, 'k--')
-  plot(T_RESP_ACC-3500, sdfMoveAcc(cc,:), 'r-', 'LineWidth',0.5)
-  plot(T_RESP_FAST-3500, sdfMoveFast(cc,:), '-', 'Color',[0 .7 0], 'LineWidth',0.5)
-  plot(-RTAcc(cc)*ones(1,2), yLim, 'r:', 'LineWidth',0.5)
-  plot(-RTFast(cc)*ones(1,2), yLim, ':', 'Color',[0 .7 0], 'LineWidth',0.5)
-  xlim([T_RESP_ACC(1) T_RESP_ACC(end)]-3500)
-  set(gca, 'YAxisLocation','right')
-  
-  ppretty([8,3])
-  pause(); close()
-  
+  %plot individual cell activity
+  plotVisRespCC(T_STIM, T_RESP, visResp(cc), sdfMove(cc), RT(cc), latVR(cc))
+  print_session_unit(gca , ninfo(cc), binfo(kk), 'horizontal'); pause()
 end%for:cells(cc)
 
 
-%% Plotting - Across cells
-NUM_CELLS_PLOT = sum(ismember({ninfo.visType}, {'sustained'}));
-
-%remove cells with no visual response
-ccRemove = ~ismember({ninfo.visType}, {'sustained'});
-ninfo(ccRemove) = [];
-visRespAcc(ccRemove,:) = [];
-visRespFast(ccRemove,:) = [];
-sdfMoveAcc(ccRemove,:) = [];
-sdfMoveFast(ccRemove,:) = [];
-RTAcc(ccRemove) = [];
-RTFast(ccRemove) = [];
-meanBlineFast(ccRemove) = [];
-meanBlineAcc(ccRemove) = [];
-latVRFast(ccRemove) = [];
-
-%normalization
-%1. subtract off baseline
-visRespAcc = visRespAcc - repmat(meanBlineAcc', 1,length(T_STIM));
-visRespFast = visRespFast - repmat(meanBlineFast', 1,length(T_STIM));
-%2. divide by max in the Fast condition
-visRespAcc = visRespAcc ./ max(visRespFast,[],2);
-visRespFast = visRespFast ./ max(visRespFast,[],2);
-
-%sort neurons by visual response latency
-[latVRFast,idxVRFast] = sort(latVRFast);
-visRespFast = visRespFast(idxVRFast,:);
-ninfo = ninfo(idxVRFast);
-
-figure(); hold on
-imagesc(T_STIM-3500, (1:NUM_CELLS_PLOT), visRespFast); %colorbar
-plot(latVRFast, (1:NUM_CELLS_PLOT), 'k.', 'MarkerSize',15)
-text((T_STIM(1)-3500)*ones(1,NUM_CELLS_PLOT), (1:NUM_CELLS_PLOT), {ninfo.sess})
-text(zeros(1,NUM_CELLS_PLOT), (1:NUM_CELLS_PLOT), {ninfo.unit})
-xlim([T_STIM(1)-5, T_STIM(end)+5] - 3500)
-ylim([0 NUM_CELLS_PLOT+1])
-ppretty([6.4,8])
-
+% %% Plotting - Across cells
+% %normalization
+% %1. subtract off baseline
+% visRespAcc = visRespAcc - repmat(meanBlineAcc', 1,length(T_STIM));
+% visRespFast = visRespFast - repmat(meanBlineFast', 1,length(T_STIM));
+% %2. divide by max in the Fast condition
+% visRespAcc = visRespAcc ./ max(visRespFast,[],2);
+% visRespFast = visRespFast ./ max(visRespFast,[],2);
+% 
+% %sort neurons by visual response latency
+% [latVRFast,idxVRFast] = sort(latVRFast);
+% visRespFast = visRespFast(idxVRFast,:);
+% ninfo = ninfo(idxVRFast);
+% 
 % figure(); hold on
-% plot(T_STIM-3500, visRespAcc, 'r-', 'LineWidth',0.5)
-% plot(T_STIM-3500, visRespFast, '-', 'Color',[0 .7 0], 'LineWidth',0.5)
-% % plot(nanmean(RTAcc)*ones(1,2), [.2 .8], 'r:', 'LineWidth',0.5)
-% % plot(nanmean(RTFast)*ones(1,2), [.2 .8], ':', 'Color',[0 .7 0], 'LineWidth',0.5)
-% xlim([T_STIM(1) T_STIM(end)]-3500)
-% ppretty([6.4,4])
+% imagesc(T_STIM-3500, (1:NUM_CELLS), visRespFast); %colorbar
+% plot(latVRFast, (1:NUM_CELLS), 'k.', 'MarkerSize',15)
+% text((T_STIM(1)-3500)*ones(1,NUM_CELLS), (1:NUM_CELLS), {ninfo.sess})
+% text(zeros(1,NUM_CELLS), (1:NUM_CELLS), {ninfo.unit})
+% xlim([T_STIM(1)-5, T_STIM(end)+5] - 3500)
+% ylim([0 NUM_CELLS+1])
+% ppretty([6.4,8])
+% 
+% % figure(); hold on
+% % plot(T_STIM-3500, visRespAcc, 'r-', 'LineWidth',0.5)
+% % plot(T_STIM-3500, visRespFast, '-', 'Color',[0 .7 0], 'LineWidth',0.5)
+% % % plot(nanmean(RTAcc)*ones(1,2), [.2 .8], 'r:', 'LineWidth',0.5)
+% % % plot(nanmean(RTFast)*ones(1,2), [.2 .8], ':', 'Color',[0 .7 0], 'LineWidth',0.5)
+% % xlim([T_STIM(1) T_STIM(end)]-3500)
+% % ppretty([6.4,4])
 
 end%fxn:plotVisRespSAT()
+
+function [] = plotVisRespCC(T_STIM, T_RESP, visResp, sdfMove, RT, latVR)
+
+figure()
+
+tmp = [visResp.Acc ; visResp.Fast ; sdfMove.Acc ; sdfMove.Fast];
+yLim = [min(tmp) max(tmp)];
+
+%visual response
+subplot(1,2,1); hold on
+plot([0 0], yLim, 'k--')
+plot(T_STIM-3500, visResp.Acc, 'r-', 'LineWidth',0.5)
+plot(T_STIM-3500, visResp.Fast, '-', 'Color',[0 .7 0], 'LineWidth',0.5)
+plot(latVR.Acc*ones(1,2), yLim, 'r:', 'LineWidth',0.5)
+plot(latVR.Fast*ones(1,2), yLim, ':', 'Color',[0 .7 0], 'LineWidth',0.5)
+% plot([T_STIM(1) T_STIM(end)]-3500, meanBlineFast(cc)*ones(1,2), 'k:', 'LineWidth',0.5)
+%   plot([T_STIM(1) T_STIM(end)]-3500, THRESH_TOP*ones(1,2), 'k--', 'LineWidth',0.5)
+%   plot([T_STIM(1) T_STIM(end)]-3500, THRESH_BOTTOM*ones(1,2), 'k--', 'LineWidth',0.5)
+plot(RT.Acc*ones(1,2), yLim, 'r:', 'LineWidth',0.5)
+plot(RT.Fast*ones(1,2), yLim, ':', 'Color',[0 .7 0], 'LineWidth',0.5)
+xlim([T_STIM(1) T_STIM(end)]-3500)
+
+%activity from primary response
+subplot(1,2,2); hold on
+plot([0 0], yLim, 'k--')
+plot(T_RESP.Acc-3500, sdfMove.Acc, 'r-', 'LineWidth',0.5)
+plot(T_RESP.Fast-3500, sdfMove.Fast, '-', 'Color',[0 .7 0], 'LineWidth',0.5)
+plot(-RT.Acc*ones(1,2), yLim, 'r:', 'LineWidth',0.5)
+plot(-RT.Fast*ones(1,2), yLim, ':', 'Color',[0 .7 0], 'LineWidth',0.5)
+xlim([T_RESP.Acc(1) T_RESP.Acc(end)]-3500)
+set(gca, 'YAxisLocation','right')
+
+ppretty([8,3])
+
+end%util:plotVisRespCC()
 
 function [ latency ] = computeVisRespLatency(visResp, bline, SD_THRESH_VR, MIN_DUR_VR, T_STIM)
 
