a8b1d09e0f32f549c3c6c951ccf33fb8435a32c4
Thomas Reppert <thomas.reppert@gmail.com>
2019-05-10 16:31:08 -0500

    Improved analysis of timing errors
    Added functionality for plotting timing error- and reward prediction error-related activity (i.e. activity after mis-timed saccades). Next step is to quantify the signal.

--- Util-Behav/determine_time_reward_SAT.m
+++ Util-Behav/determine_time_reward_SAT.m
@@ -11,21 +11,21 @@ for kk = 1:NUM_SESSIONS
   RT_kk = double(binfo(kk).resptime);
   rewtime_kk = binfo(kk).rewtime;
   
-  idx_corr = ~(binfo(kk).err_dir | binfo(kk).err_time | binfo(kk).err_hold);
-  idx_errtime = (~binfo(kk).err_dir & binfo(kk).err_time);
-  idx_errdir = (binfo(kk).err_dir & ~binfo(kk).err_time);
+  idxCorr = ~(binfo(kk).err_dir | binfo(kk).err_time | binfo(kk).err_hold);
+  idxErr = (binfo(kk).err_dir | binfo(kk).err_time | binfo(kk).err_hold);
+%   idx_errtime = (~binfo(kk).err_dir & binfo(kk).err_time);
+%   idx_errdir = (binfo(kk).err_dir & ~binfo(kk).err_time);
   
   %get estimate of expected time of reward on error trials
   med_t_rew_kk = round(nanmedian(rewtime_kk));
   
-  binfo(kk).rewtime(idx_corr) = rewtime_kk(idx_corr) - RT_kk(idx_corr);
-  binfo(kk).rewtime(idx_errtime) = med_t_rew_kk - RT_kk(idx_errtime);
-  binfo(kk).rewtime(idx_errdir) = med_t_rew_kk - RT_kk(idx_errdir);
-  binfo(kk).rewtime(~(idx_corr | idx_errtime | idx_errdir)) = NaN;
+  binfo(kk).rewtime(idxCorr) = rewtime_kk(idxCorr) - RT_kk(idxCorr);
+  binfo(kk).rewtime(idxErr) = med_t_rew_kk - RT_kk(idxErr);
+%   binfo(kk).rewtime(~(idxCorr | idxErr)) = NaN;
   
   %make sure times are reasonable
-  idx_nan = ((binfo(kk).rewtime < LIM_TREW(1)) | (binfo(kk).rewtime > LIM_TREW(2)));
-  binfo(kk).rewtime(idx_nan) = NaN;
+  idxNan = ((binfo(kk).rewtime < LIM_TREW(1)) | (binfo(kk).rewtime > LIM_TREW(2)));
+  binfo(kk).rewtime(idxNan) = NaN;
   
 end%for:sessions(kk)
 
