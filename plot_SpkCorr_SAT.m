2b12e38cce99191931e57a14ae64278c12b0e1b0
Thomas Reppert <thomas.reppert@gmail.com>
2019-08-09 14:06:12 -0500

    Update plot_SpkCorr_SAT.m
    Updated function to:
    1. Split on Fast/Accurate condition
    2. Fit the spike count VS. trial relationship in order to control for the effect of non-constant spike isolation during the recording session.
    3. Output neuron types, split on whether spike count correlation was significant or not.

--- plot_SpkCorr_SAT.m
+++ plot_SpkCorr_SAT.m
@@ -15,6 +15,11 @@ T_BASE = 3500 + [-600 20]; %baseline (re. stimulus)
 T_VIS  = 3500 +  [75 200]; %visual response (re. stimulus)
 T_ERR  = 3500 + [100 300]; %error-related (re. saccade)
 
+%function for curve fitting
+f_Fit = @(x,xdata) x(1) + x(2)*xdata + x(3)*xdata.^2 + x(4)*xdata.^3;
+p0 = [5 0 0 0]; %fit parameter initialization
+opt_ = optimset('Display','off'); %turn off messages from lsqcurvefit
+
 %% Isolate all pairs of SEF & SC neurons
 iiPairKeep = [];
 
@@ -45,9 +50,15 @@ nPairDB = nPairDB(iiPairKeep,:);
 %% Compute spike counts for intervals of interest
 N_PAIR = size(nPairDB, 1);
 
-logPVal_Base = NaN(1,N_PAIR);
-logPVal_Vis  = NaN(1,N_PAIR);
-logPVal_Err  = NaN(1,N_PAIR);
+logPVal_Acc_Base = NaN(1,N_PAIR); logPVal_Fast_Base = NaN(1,N_PAIR);
+logPVal_Acc_Vis  = NaN(1,N_PAIR); logPVal_Fast_Vis  = NaN(1,N_PAIR);
+logPVal_Acc_Err  = NaN(1,N_PAIR); logPVal_Fast_Err  = NaN(1,N_PAIR);
+
+Pair_UID = cell(N_PAIR,1);
+X_Type = cell(N_PAIR,1);
+Y_Type = cell(N_PAIR,1);
+X_Area = cell(N_PAIR,1);
+Y_Area = cell(N_PAIR,1);
 
 for ii = 1:N_PAIR
   
@@ -56,7 +67,32 @@ for ii = 1:N_PAIR
   
   X_spikes = spikes(X_unitNum).SAT;
   Y_spikes = spikes(Y_unitNum).SAT;
-    
+  
+  %save information on neuron type
+  if (nPairDB.X_visGrade(ii) >= 2)
+    X_Type{ii} = cat(2, X_Type{ii}, 'Vis');
+  end
+  if (nPairDB.X_moveGrade(ii) >= 2)
+    X_Type{ii} = cat(2, X_Type{ii}, 'Move');
+  end
+  if (nPairDB.X_errGrade(ii) >= 2)
+    X_Type{ii} = cat(2, X_Type{ii}, 'Err');
+  end
+  if (nPairDB.Y_visGrade(ii) >= 2)
+    Y_Type{ii} = cat(2, Y_Type{ii}, 'Vis');
+  end
+  if (nPairDB.Y_moveGrade(ii) >= 2)
+    Y_Type{ii} = cat(2, Y_Type{ii}, 'Move');
+  end
+  if (nPairDB.Y_errGrade(ii) >= 2)
+    Y_Type{ii} = cat(2, Y_Type{ii}, 'Err');
+  end
+  
+  %save pair UID and neuron area
+  Pair_UID{ii} = nPairDB.Pair_UID{ii};
+  X_Area{ii} = nPairDB.X_area{ii};
+  Y_Area{ii} = nPairDB.Y_area{ii};
+  
   kk = ismember({binfo.session}, nPairDB.X_sess(ii));
   
   %index by isolation quality
@@ -65,9 +101,14 @@ for ii = 1:N_PAIR
   %index by condition
   idxAcc = (binfo(kk).condition == 1);
   idxFast = (binfo(kk).condition == 3);
+  %index by trial oucome
+%   idxCorr = ~(binfo(kk).err_dir | binfo(kk).err_time | binfo(kk).err_hold | binfo(kk).err_nosacc);
+%   idxErr = (binfo(kk).err_dir & ~binfo(kk).err_time);
   
   trialAcc = find(idxAcc & ~(X_idxIso | Y_idxIso));
   trialFast = find(idxFast & ~(X_idxIso | Y_idxIso));
+%   trialAcc = find(idxAcc & idxErr & ~(X_idxIso | Y_idxIso));
+%   trialFast = find(idxFast & idxErr & ~(X_idxIso | Y_idxIso));
   
   X_Acc_spikes = X_spikes(trialAcc);   X_Fast_spikes = X_spikes(trialFast);
   Y_Acc_spikes = Y_spikes(trialAcc);   Y_Fast_spikes = Y_spikes(trialFast);
@@ -108,87 +149,232 @@ for ii = 1:N_PAIR
   end
   %****************************************
   
+  %FIT THE SPIKE COUNT VS. TRIAL NUMBER DATA
   %combine data across conditions
+  trialAll = [trialAcc trialFast];
   X_sp_Base = [X_Acc_sp_Base X_Fast_sp_Base];   Y_sp_Base = [Y_Acc_sp_Base Y_Fast_sp_Base];
   X_sp_Vis = [X_Acc_sp_Vis X_Fast_sp_Vis];      Y_sp_Vis = [Y_Acc_sp_Vis Y_Fast_sp_Vis];
   X_sp_Err = [X_Acc_sp_Err X_Fast_sp_Err];      Y_sp_Err = [Y_Acc_sp_Err Y_Fast_sp_Err];
   
+  p_X_Base = lsqcurvefit(f_Fit, p0, trialAll, X_sp_Base, [], [], opt_);
+  p_Y_Base = lsqcurvefit(f_Fit, p0, trialAll, Y_sp_Base, [], [], opt_);
+  p_X_Vis = lsqcurvefit(f_Fit, p0, trialAll, X_sp_Vis, [], [], opt_);
+  p_Y_Vis = lsqcurvefit(f_Fit, p0, trialAll, Y_sp_Vis, [], [], opt_);
+  p_X_Err = lsqcurvefit(f_Fit, p0, trialAll, X_sp_Err, [], [], opt_);
+  p_Y_Err = lsqcurvefit(f_Fit, p0, trialAll, Y_sp_Err, [], [], opt_);
+  
+  %COMPUTE FIT RESIDUALS
+  X_Acc_sp_Base_Res = X_Acc_sp_Base - f_Fit(p_X_Base,trialAcc);
+  Y_Acc_sp_Base_Res = Y_Acc_sp_Base - f_Fit(p_Y_Base,trialAcc);
+  X_Acc_sp_Vis_Res = X_Acc_sp_Vis - f_Fit(p_X_Vis,trialAcc);
+  Y_Acc_sp_Vis_Res = Y_Acc_sp_Vis - f_Fit(p_Y_Vis,trialAcc);
+  X_Acc_sp_Err_Res = X_Acc_sp_Err - f_Fit(p_X_Err,trialAcc);
+  Y_Acc_sp_Err_Res = Y_Acc_sp_Err - f_Fit(p_Y_Err,trialAcc);
+  X_Fast_sp_Base_Res = X_Fast_sp_Base - f_Fit(p_X_Base,trialFast);
+  Y_Fast_sp_Base_Res = Y_Fast_sp_Base - f_Fit(p_Y_Base,trialFast);
+  X_Fast_sp_Vis_Res = X_Fast_sp_Vis - f_Fit(p_X_Vis,trialFast);
+  Y_Fast_sp_Vis_Res = Y_Fast_sp_Vis - f_Fit(p_Y_Vis,trialFast);
+  X_Fast_sp_Err_Res = X_Fast_sp_Err - f_Fit(p_X_Err,trialFast);
+  Y_Fast_sp_Err_Res = Y_Fast_sp_Err - f_Fit(p_Y_Err,trialFast);
+  
+  
   %COMPUTE SPEARMAN RANK CORRELATION COEFFICIENT AND P-VALUE
-  [rho_Base, pval_Base] = corr(X_sp_Base', Y_sp_Base', 'Type','Spearman');
-  [rho_Vis,  pval_Vis]  = corr(X_sp_Vis', Y_sp_Vis', 'Type','Spearman');
-  [rho_Err,  pval_Err]  = corr(X_sp_Err', Y_sp_Err', 'Type','Spearman');
+%   [rho_Acc_Base, pval_Acc_Base] = corr(X_Acc_sp_Base', Y_Acc_sp_Base', 'Type','Spearman');
+%   [rho_Acc_Vis,  pval_Acc_Vis]  = corr(X_Acc_sp_Vis',  Y_Acc_sp_Vis', 'Type','Spearman');
+%   [rho_Acc_Err,  pval_Acc_Err]  = corr(X_Acc_sp_Err',  Y_Acc_sp_Err', 'Type','Spearman');
+%   [rho_Fast_Base, pval_Fast_Base] = corr(X_Fast_sp_Base', Y_Fast_sp_Base', 'Type','Spearman');
+%   [rho_Fast_Vis,  pval_Fast_Vis]  = corr(X_Fast_sp_Vis',  Y_Fast_sp_Vis', 'Type','Spearman');
+%   [rho_Fast_Err,  pval_Fast_Err]  = corr(X_Fast_sp_Err',  Y_Fast_sp_Err', 'Type','Spearman');
+  [rho_Acc_Base, pval_Acc_Base] = corr(X_Acc_sp_Base_Res', Y_Acc_sp_Base_Res', 'Type','Spearman');
+  [rho_Acc_Vis,  pval_Acc_Vis]  = corr(X_Acc_sp_Vis_Res',  Y_Acc_sp_Vis_Res', 'Type','Spearman');
+  [rho_Acc_Err,  pval_Acc_Err]  = corr(X_Acc_sp_Err_Res',  Y_Acc_sp_Err_Res', 'Type','Spearman');
+  [rho_Fast_Base, pval_Fast_Base] = corr(X_Fast_sp_Base_Res', Y_Fast_sp_Base_Res', 'Type','Spearman');
+  [rho_Fast_Vis,  pval_Fast_Vis]  = corr(X_Fast_sp_Vis_Res',  Y_Fast_sp_Vis_Res', 'Type','Spearman');
+  [rho_Fast_Err,  pval_Fast_Err]  = corr(X_Fast_sp_Err_Res',  Y_Fast_sp_Err_Res', 'Type','Spearman');
   
   %save (signed) rank correlation p-value
-  logPVal_Base(ii) = sign(rho_Base) * -log(pval_Base);
-  logPVal_Vis(ii)  = sign(rho_Vis)  * -log(pval_Vis);
-  logPVal_Err(ii)  = sign(rho_Err)  * -log(pval_Err);
+  logPVal_Acc_Base(ii) = sign(rho_Acc_Base) * -log(pval_Acc_Base);
+  logPVal_Acc_Vis(ii)  = sign(rho_Acc_Vis)  * -log(pval_Acc_Vis);
+  logPVal_Acc_Err(ii)  = sign(rho_Acc_Err)  * -log(pval_Acc_Err);
+  logPVal_Fast_Base(ii) = sign(rho_Fast_Base) * -log(pval_Fast_Base);
+  logPVal_Fast_Vis(ii)  = sign(rho_Fast_Vis)  * -log(pval_Fast_Vis);
+  logPVal_Fast_Err(ii)  = sign(rho_Fast_Err)  * -log(pval_Fast_Err);
   
-  %PLOT CORRELATION AND SPIKE COUNT VS. TRIAL NUMBER
+  
+  
+  %PLOT CORRELATION
 %   figure()
+%   %*********************************
+%   %Correlations - Accurate condition
+%   subplot(2,3,1); hold on %Baseline
+%   title(['Baseline: R_{Sp} = ', num2str(rho_Acc_Base), '  p_{Sp} = ', num2str(pval_Acc_Base)], 'FontSize',9)
+%   scatter(X_Acc_sp_Base_Res, Y_Acc_sp_Base_Res, 25, 'r', 'filled', 'MarkerFaceAlpha',0.1)
+%   ylabel([nPairDB.X_sess{ii},'-',nPairDB.Y_unit{ii}, ' (',nPairDB.Y_area{ii},')', ' (sp/s)'])
 %   
-%   subplot(2,3,1); hold on %correlation -- Baseline
-%   title([nPairDB.X_sess{ii}, ' -- Baseline: R_{Sp} = ', num2str(rho_Base), '  p_{Sp} = ', num2str(pval_Base)], 'FontSize',8)
-%   scatter(X_sp_Base, Y_sp_Base, 25, 'k', 'filled', 'MarkerFaceAlpha',0.3)
-%   xlabel([nPairDB.X_unit{ii}, ' (',nPairDB.X_area{ii},')', ' (sp/s)'])
-%   ylabel([nPairDB.Y_unit{ii}, ' (',nPairDB.Y_area{ii},')', ' (sp/s)'])
+%   subplot(2,3,2); hold on %Visual Response
+%   title(['Vis. response: R_{Sp} = ', num2str(rho_Acc_Vis), '  p_{Sp} = ', num2str(pval_Acc_Vis)], 'FontSize',9)
+%   scatter(X_Acc_sp_Vis_Res, Y_Acc_sp_Vis_Res, 25, 'r', 'filled', 'MarkerFaceAlpha',0.1)
 %   
-%   subplot(2,3,2); hold on %correlation -- Visual Response
-%   title(['Vis. response: R_{Sp} = ', num2str(rho_Vis), '  p_{Sp} = ', num2str(pval_Vis)], 'FontSize',8)
-%   scatter(X_sp_Vis, Y_sp_Vis, 25, 'k', 'filled', 'MarkerFaceAlpha',0.3)
-%   xlabel([nPairDB.X_unit{ii}, ' (',nPairDB.X_area{ii},')', ' (sp/s)'])
-%   ylabel([nPairDB.Y_unit{ii}, ' (',nPairDB.Y_area{ii},')', ' (sp/s)'])
+%   subplot(2,3,3); hold on %Error Period
+%   title(['Post-response: R_{Sp} = ', num2str(rho_Acc_Err), '  p_{Sp} = ', num2str(pval_Acc_Err)], 'FontSize',9)
+%   scatter(X_Acc_sp_Err_Res, Y_Acc_sp_Err_Res, 25, 'r', 'filled', 'MarkerFaceAlpha',0.1)
+%   %*********************************
 %   
-%   subplot(2,3,3); hold on %correlation -- Error Period
-%   title(['Post-response: R_{Sp} = ', num2str(rho_Err), '  p_{Sp} = ', num2str(pval_Err)], 'FontSize',8)
-%   scatter(X_sp_Err, Y_sp_Err, 25, 'k', 'filled', 'MarkerFaceAlpha',0.3)
-%   xlabel([nPairDB.X_unit{ii}, ' (',nPairDB.X_area{ii},')', ' (sp/s)'])
-%   ylabel([nPairDB.Y_unit{ii}, ' (',nPairDB.Y_area{ii},')', ' (sp/s)'])
+%   %*********************************
+%   %Correlations - Fast condition
+%   subplot(2,3,4); hold on %Baseline
+%   title(['Baseline: R_{Sp} = ', num2str(rho_Fast_Base), '  p_{Sp} = ', num2str(pval_Fast_Base)], 'FontSize',9)
+%   scatter(X_Fast_sp_Base_Res, Y_Fast_sp_Base_Res, 25, [0 .7 0], 'filled', 'MarkerFaceAlpha',0.1)
+%   xlabel([nPairDB.X_sess{ii},'-',nPairDB.X_unit{ii}, ' (',nPairDB.X_area{ii},')', ' (sp/s)'])
+%   ylabel([nPairDB.X_sess{ii},'-',nPairDB.Y_unit{ii}, ' (',nPairDB.Y_area{ii},')', ' (sp/s)'])
 %   
-%   subplot(2,3,4); hold on %spike count vs. trial -- Baseline
-%   scatter(trialAcc, X_Acc_sp_Base, 25, 'r', 'filled');
-%   scatter(trialFast, X_Fast_sp_Base, 25, [0 .7 0], 'filled');
-%   scatter(trialAcc, Y_Acc_sp_Base, 25, [.5 0 0], 'filled');
-%   scatter(trialFast, Y_Fast_sp_Base, 25, [0 .4 0], 'filled');
-%   legend({nPairDB.X_unit{ii} nPairDB.X_unit{ii} nPairDB.Y_unit{ii} nPairDB.Y_unit{ii}}, ...
-%     'Location','north', 'Orientation','horizontal')
-%   xlabel('Trial number'); ylabel('Activity (sp/s)')
+%   subplot(2,3,5); hold on %Visual Response
+%   title(['Vis. response: R_{Sp} = ', num2str(rho_Fast_Vis), '  p_{Sp} = ', num2str(pval_Fast_Vis)], 'FontSize',9)
+%   scatter(X_Fast_sp_Vis_Res, Y_Fast_sp_Vis_Res, 25, [0 .7 0], 'filled', 'MarkerFaceAlpha',0.1)
+%   xlabel([nPairDB.X_sess{ii},'-',nPairDB.X_unit{ii}, ' (',nPairDB.X_area{ii},')', ' (sp/s)'])
 %   
-%   subplot(2,3,5); hold on %spike count vs. trial -- Visual Response
-%   scatter(trialAcc, X_Acc_sp_Vis, 25, 'r', 'filled');
-%   scatter(trialFast, X_Fast_sp_Vis, 25, [0 .7 0], 'filled');
-%   scatter(trialAcc, Y_Acc_sp_Vis, 25, [.5 0 0], 'filled');
-%   scatter(trialFast, Y_Fast_sp_Vis, 25, [0 .4 0], 'filled');
-%   legend({nPairDB.X_unit{ii} nPairDB.X_unit{ii} nPairDB.Y_unit{ii} nPairDB.Y_unit{ii}}, ...
-%     'Location','north', 'Orientation','horizontal')
-%   xlabel('Trial number'); ylabel('Activity (sp/s)')
+%   subplot(2,3,6); hold on %Error Period
+%   title(['Post-response: R_{Sp} = ', num2str(rho_Fast_Err), '  p_{Sp} = ', num2str(pval_Fast_Err)], 'FontSize',9)
+%   scatter(X_Fast_sp_Err_Res, Y_Fast_sp_Err_Res, 25, [0 .7 0], 'filled', 'MarkerFaceAlpha',0.1)
+%   xlabel([nPairDB.X_sess{ii},'-',nPairDB.X_unit{ii}, ' (',nPairDB.X_area{ii},')', ' (sp/s)'])
+%   %*********************************
+%   ppretty([18,7]); pause(0.25)
+  
+  
+%   %PLOT SPIKE COUNT VS. TRIAL NUMBER
+%   trialLim = [min(trialAll) max(trialAll)];
 %   
-%   subplot(2,3,6); hold on %spike count vs. trial -- Error Period
-%   scatter(trialAcc, X_Acc_sp_Err, 25, 'r', 'filled');
-%   scatter(trialFast, X_Fast_sp_Err, 25, [0 .7 0], 'filled');
-%   scatter(trialAcc, Y_Acc_sp_Err, 25, [.5 0 0], 'filled');
-%   scatter(trialFast, Y_Fast_sp_Err, 25, [0 .4 0], 'filled');
-%   legend({nPairDB.X_unit{ii} nPairDB.X_unit{ii} nPairDB.Y_unit{ii} nPairDB.Y_unit{ii}}, ...
-%     'Location','north', 'Orientation','horizontal')
-%   xlabel('Trial number'); ylabel('Activity (sp/s)')
+%   figure()
+%   %*********************************
+%   %Spike count vs. trial number -- Neuron X
+%   subplot(4,3,1); hold on %Baseline
+%   title([nPairDB.X_sess{ii},'-',nPairDB.X_unit{ii},'-',nPairDB.X_area{ii}], 'FontSize',12)
+%   scatter(trialAcc, X_Acc_sp_Base, 20, 'r', 'filled');
+%   scatter(trialFast, X_Fast_sp_Base, 20, [0 .7 0], 'filled');
+%   plot(sort(trialAll), f_Fit(p_X_Base,sort(trialAll)), 'k-', 'LineWidth',1.5)
+%   ylabel('Activity (sp/s)')
 %   
-%   ppretty([18,7])
+%   subplot(4,3,2); hold on %Visual Response
+%   title([nPairDB.X_sess{ii},'-',nPairDB.X_unit{ii},'-',nPairDB.X_area{ii}], 'FontSize',12)
+%   scatter(trialAcc, X_Acc_sp_Vis, 20, 'r', 'filled');
+%   scatter(trialFast, X_Fast_sp_Vis, 20, [0 .7 0], 'filled');
+%   plot(sort(trialAll), f_Fit(p_X_Vis,sort(trialAll)), 'k-', 'LineWidth',1.5)
 %   
+%   subplot(4,3,3); hold on %Error Period
+%   title([nPairDB.X_sess{ii},'-',nPairDB.X_unit{ii},'-',nPairDB.X_area{ii}], 'FontSize',12)
+%   scatter(trialAcc, X_Acc_sp_Err, 20, 'r', 'filled');
+%   scatter(trialFast, X_Fast_sp_Err, 20, [0 .7 0], 'filled');
+%   plot(sort(trialAll), f_Fit(p_X_Err,sort(trialAll)), 'k-', 'LineWidth',1.5)
+%   %*********************************
+%   
+%   %*********************************
+%   %RESIDUAL vs. trial number -- Neuron X
+%   subplot(4,3,4); hold on %Baseline
+%   scatter(trialAcc, X_Acc_sp_Base_Res, 20, 'r', 'filled');
+%   scatter(trialFast, X_Fast_sp_Base_Res, 20, [0 .7 0], 'filled');
+%   plot(trialLim, [0 0], 'k-', 'LineWidth',1.5)
+%   ylabel('Residual (sp/s)')
+%   
+%   subplot(4,3,5); hold on %Visual Response
+%   scatter(trialAcc, X_Acc_sp_Vis_Res, 20, 'r', 'filled');
+%   scatter(trialFast, X_Fast_sp_Vis_Res, 20, [0 .7 0], 'filled');
+%   plot(trialLim, [0 0], 'k-', 'LineWidth',1.5)
+%   
+%   subplot(4,3,6); hold on %Error Period
+%   scatter(trialAcc, X_Acc_sp_Err_Res, 20, 'r', 'filled');
+%   scatter(trialFast, X_Fast_sp_Err_Res, 20, [0 .7 0], 'filled');
+%   plot(trialLim, [0 0], 'k-', 'LineWidth',1.5)
+%   %*********************************
+% 
+%   
+%   %*********************************
+%   %Spike count vs. trial number -- Neuron Y
+%   subplot(4,3,7); hold on %Baseline
+%   title([nPairDB.X_sess{ii},'-',nPairDB.Y_unit{ii},'-',nPairDB.Y_area{ii}], 'FontSize',12)
+%   scatter(trialAcc, Y_Acc_sp_Base, 20, 'r', 'filled');
+%   scatter(trialFast, Y_Fast_sp_Base, 20, [0 .7 0], 'filled');
+%   plot(sort(trialAll), f_Fit(p_Y_Base,sort(trialAll)), 'k-', 'LineWidth',1.5)
+%   ylabel('Activity (sp/s)')
+%   
+%   subplot(4,3,8); hold on %Visual Response
+%   title([nPairDB.X_sess{ii},'-',nPairDB.Y_unit{ii},'-',nPairDB.Y_area{ii}], 'FontSize',12)
+%   scatter(trialAcc, Y_Acc_sp_Vis, 20, 'r', 'filled');
+%   scatter(trialFast, Y_Fast_sp_Vis, 20, [0 .7 0], 'filled');
+%   plot(sort(trialAll), f_Fit(p_Y_Vis,sort(trialAll)), 'k-', 'LineWidth',1.5)
+%   
+%   subplot(4,3,9); hold on %Error Period
+%   title([nPairDB.X_sess{ii},'-',nPairDB.Y_unit{ii},'-',nPairDB.Y_area{ii}], 'FontSize',12)
+%   scatter(trialAcc, Y_Acc_sp_Err, 20, 'r', 'filled');
+%   scatter(trialFast, Y_Fast_sp_Err, 20, [0 .7 0], 'filled');
+%   plot(sort(trialAll), f_Fit(p_Y_Err,sort(trialAll)), 'k-', 'LineWidth',1.5)
+%   %*********************************
+%   
+%   %*********************************
+%   %RESIDUAL vs. trial number -- Neuron Y
+%   subplot(4,3,10); hold on %Baseline
+%   scatter(trialAcc, Y_Acc_sp_Base_Res, 20, 'r', 'filled');
+%   scatter(trialFast, Y_Fast_sp_Base_Res, 20, [0 .7 0], 'filled');
+%   plot(trialLim, [0 0], 'k-', 'LineWidth',1.5)
+%   xlabel('Trial number'); ylabel('Residual (sp/s)')
+%   
+%   subplot(4,3,11); hold on %Visual Response
+%   scatter(trialAcc, Y_Acc_sp_Vis_Res, 20, 'r', 'filled');
+%   scatter(trialFast, Y_Fast_sp_Vis_Res, 20, [0 .7 0], 'filled');
+%   plot(trialLim, [0 0], 'k-', 'LineWidth',1.5)
+%   xlabel('Trial number')
+%   
+%   subplot(4,3,12); hold on %Error Period
+%   scatter(trialAcc, Y_Acc_sp_Err_Res, 20, 'r', 'filled');
+%   scatter(trialFast, Y_Fast_sp_Err_Res, 20, [0 .7 0], 'filled');
+%   plot(trialLim, [0 0], 'k-', 'LineWidth',1.5)
+%   xlabel('Trial number')
+%   %*********************************
+%   ppretty([18,7]); pause(0.25)
+  
 %   pause(.05)
-%   print([DIR_PRINT, nPairDB.Pair_UID{ii}, '.tif'], '-dtiff')
+%   print([DIR_PRINT, 'Correlation-',nPairDB.Pair_UID{ii}, '.tif'], '-dtiff')
+% %   print([DIR_PRINT, 'CurveFitting-',nPairDB.Pair_UID{ii}, '.tif'], '-dtiff')
 %   pause(.05)
 %   close()
   
 end%for:cellPair(ii)
 
+%% Save table of neuron types for classification
+% idxCorr_Acc  = (logPVal_Acc_Err >= -log(.06));
+% idxCorr_Fast = (logPVal_Fast_Err >= -log(.06));
+% 
+% cellData = table(Pair_UID, X_Area, Y_Area, X_Type, Y_Type);
+% cellData_Corr_Acc = cellData(idxCorr_Acc,:);      cellData_Corr_Fast = cellData(idxCorr_Fast,:);
+% cellData_NCorr_Acc = cellData(~idxCorr_Acc,:);    cellData_NCorr_Fast = cellData(~idxCorr_Fast,:);
+% 
+% %write to Excel
+% fileName = 'cellData-SEF-SC.xlsx';
+% writetable(cellData_Corr_Acc, [DIR_PRINT, fileName], 'Sheet',1, 'Range','B3')
+% writetable(cellData_NCorr_Acc, [DIR_PRINT, fileName], 'Sheet',1, 'Range','H3')
+% writetable(cellData_Corr_Fast, [DIR_PRINT, fileName], 'Sheet',1, 'Range','N3')
+% writetable(cellData_NCorr_Fast, [DIR_PRINT, fileName], 'Sheet',1, 'Range','T3')
 
 %% Plot summary statistics for all pairs
-figure(); hold on
+figure()
+XPLOT_SCATTER = [ones(1,N_PAIR) , 2*ones(1,N_PAIR) , 3*ones(1,N_PAIR)];
+
+subplot(1,2,1); hold on %Accurate condition
 plot([.6 3.4],  -log(.05)*ones(1,2), 'k:'); plot([.6 3.4],  log(.05)*ones(1,2), 'k:')
 plot([.6 3.4],  -log(.01)*ones(1,2), 'k:'); plot([.6 3.4],  log(.01)*ones(1,2), 'k:')
 plot([.6 3.4], -log(.001)*ones(1,2), 'k:'); plot([.6 3.4], log(.001)*ones(1,2), 'k:')
-plot((1:3), [logPVal_Base ; logPVal_Vis ; logPVal_Err]', 'k.-', 'MarkerSize',15)
+scatter(XPLOT_SCATTER, [logPVal_Acc_Base , logPVal_Acc_Vis , logPVal_Acc_Err], 20, 'r', 'filled', 'MarkerFaceAlpha',0.4)
+plot((1:3), [logPVal_Acc_Base ; logPVal_Acc_Vis ; logPVal_Acc_Err]', 'r-')
 xticks(1:3); xticklabels({'Base','Vis','Err'}); ylabel('-log(p)')
-ppretty([6.4,4]); set(gca, 'XMinorTick','off')
+
+subplot(1,2,2); hold on %Fast condition
+plot([.6 3.4],  -log(.05)*ones(1,2), 'k:'); plot([.6 3.4],  log(.05)*ones(1,2), 'k:')
+plot([.6 3.4],  -log(.01)*ones(1,2), 'k:'); plot([.6 3.4],  log(.01)*ones(1,2), 'k:')
+plot([.6 3.4], -log(.001)*ones(1,2), 'k:'); plot([.6 3.4], log(.001)*ones(1,2), 'k:')
+scatter(XPLOT_SCATTER, [logPVal_Fast_Base , logPVal_Fast_Vis , logPVal_Fast_Err], 20, [0 .7 0], 'filled', 'MarkerFaceAlpha',0.4)
+plot((1:3), [logPVal_Fast_Base ; logPVal_Fast_Vis ; logPVal_Fast_Err]', '-', 'Color',[0 .7 0])
+xticks(1:3); xticklabels({'Base','Vis','Err'}); ylabel('-log(p)')
+
+ppretty([9,4]); set(gca, 'XMinorTick','off')
 
 
 end%fxn:myFunction()
