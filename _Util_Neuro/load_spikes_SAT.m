5dcdc43200b53feb6846dd741e0d40cac3da9d42
Thomas Reppert <thomas.reppert@gmail.com>
2023-06-25 18:58:10 -0500

    Update load_spikes_SAT.m
    Include code for loading memory-guided (MG) spike times.

--- _Util_Neuro/load_spikes_SAT.m
+++ _Util_Neuro/load_spikes_SAT.m
@@ -1,8 +1,13 @@
 function [ spikes ] = load_spikes_SAT( uNum , varargin )
-%load_spikes_SAT Summary of this function goes here
-%   Detailed explanation goes here
+%load_spikes_SAT This function loads the pre-processed spike times for the
+%neuron of interest from the SAT data set (Da, Eu, Q, S).
+% 
+%   Inputs
+%   uNum - unit identification number
+%   task - SAT (search), MG (mem-guided), DET (detection)
+% 
 
-args = getopt(varargin, {{'user=','thoma'}});
+args = getopt(varargin, {{'user=','thoma'}, {'task=','SAT'}});
 
 if isempty(args.user)
   user = 'Thomas Reppert';
@@ -10,7 +15,7 @@ else
   user = args.user;
 end
 
-dataDir = ['C:\Users\', user, '\Dropbox\Speed Accuracy\Data\spikes\'];
+dataDir = ['C:\Users\', user, '\Dropbox\Speed Accuracy\Data\spikes', args.task, '\'];
 
 str_uu = num2str(uNum);
 if (uNum < 10)
@@ -19,8 +24,13 @@ elseif (uNum < 100)
   str_uu = ['0', str_uu];
 end
 
-spikes = load([dataDir, 'spikes', str_uu, '.mat'], 'spikes');
-spikes = spikes.spikes;
+if strcmp(args.task, 'MG')
+  spikes = load([dataDir, 'spikesMG_', str_uu, '.mat'], ['spikesMG',str_uu]);
+  spikes = spikes.(['spikesMG',str_uu]);
+else
+  spikes = load([dataDir, 'spikes', str_uu, '.mat'], 'spikes');
+  spikes = spikes.spikes;
+end
 
 end % util : load_spikes_SAT()
 
