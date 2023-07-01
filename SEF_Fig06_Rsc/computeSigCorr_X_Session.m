fe9a70481766eb0e2aadee60aad5ad761cb16a07
Thomas Reppert <thomas.reppert@gmail.com>
2023-07-01 16:46:33 -0500

    Update computeSigCorr_X_Session.m

--- SEF_Fig06_Rsc/computeSigCorr_X_Session.m
+++ SEF_Fig06_Rsc/computeSigCorr_X_Session.m
@@ -3,54 +3,56 @@
 % task-relevant neurons for a given session.
 % 
 
-idx_Area = ismember(unitData.Area, {'SEF','FEF','SC'});
-idx_Fxn = ~(unitData.FxnType == "None");
+%index behavioral data
+sessTest = find(ismember(behavData.Monkey, 'S'));
+behavTest = behavData(sessTest,:);
+nSess = numel(sessTest);
 
-nSess = 16;
+%index physiology data
+idx_Area = ismember(unitData.Area, 'FEF');
+idx_Hemi = ismember(unitData.Hemi, {'L','R'});
+idx_Fxn  = ismember(unitData.VR, 1);
+
+iVR = 2; %visual response epoch
+iPS = 3; %post-saccade epoch
 nDir = 8;
-iVR = 2; %index of visual response epoch
-iPS = 3; %index of post-saccade epoch
 
 rSignal = new_struct({'Acc','Fast'}, 'dim',[1,nSess]);
 
 for kk = 1:nSess
-  idx_Sess = ismember(unitData.SessionID, kk);
-  unitTest = unitData( idx_Sess & idx_Area & idx_Fxn , : );
+  idx_Sess = ismember(unitData.SessionID, sessTest(kk));
+  unitTest = unitData( idx_Sess & idx_Area & idx_Hemi , : );
   nUnit = size(unitTest,1);
-  
-  nTrial_kk = behavData.NumTrials(kk);
-  iIso_kk = removeTrials_Isolation(trialRemove{kk}, nTrial_kk); %poor isolation
+
+  if (nUnit == 0); continue; end
   
   sc_Acc = NaN(nDir,nUnit); %spike count - Acc condition
   sc_Fast = sc_Acc; %spike count - Fast condition
   
   for uu = 1:nUnit
     %% Compute spike counts by condition and direction
-    [scAcc_uu,scFast_uu] = computeSpikeCount_SAT(unitTest(uu,:), behavData(kk,:), 'Correct', iIso_kk);
+    [scAcc_uu,scFast_uu] = computeSpikeCount_SAT(unitTest(uu,:), behavTest(kk,:), 'Correct');
 
     %use appropriate time epoch(s) to estimate signal correlation
     if (unitTest.Area(uu) == "SEF")
       iEpoch = [iVR,iPS];
     else %FEF/SC
-      switch unitTest.FxnType(uu)
-        case "V"
-          iEpoch = iVR;
-        case "VM"
-          iEpoch = [iVR,iPS];
-        case "M"
-          iEpoch = iPS;
-      end % switch (unit fxn type)
+      iEpoch = iVR;
     end %switch (unit area)
 
-    sc_Acc(:,uu)  = mean(scAcc_uu(1:nDir,iEpoch), 2);
+    sc_Acc(:,uu)  = mean(scAcc_uu(1:nDir,iEpoch),  2);
     sc_Fast(:,uu) = mean(scFast_uu(1:nDir,iEpoch), 2);
   
   end % for : unit (uu)
   
+  %account for directions with no data
+  dnanAcc  = isnan(sc_Acc(:,1));    sc_Acc(dnanAcc,:) = [];
+  dnanFast = isnan(sc_Fast(:,1));   sc_Fast(dnanFast,:) = [];
+
   %% Compute signal correlation across units for this session
   rSignal(kk).Acc  = corr(sc_Acc,  "type","Pearson");
   rSignal(kk).Fast = corr(sc_Fast, "type","Pearson");
 
 end % for : session (kk)
 
-clearvars -except behavData unitData pairData ROOTDIR* rNoise* rSignal*
+clearvars -except ROOTDIR* behavData* unitData pairData rNoise* rSignal*
