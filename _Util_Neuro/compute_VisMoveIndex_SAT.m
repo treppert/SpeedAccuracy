417aaef39076dd6fb92214cef67a344091ab39c0
Thomas Reppert <thomas.reppert@gmail.com>
2023-07-17 18:10:08 -0500

    Create compute_VisMoveIndex_SAT.m

---
+++ _Util_Neuro/compute_VisMoveIndex_SAT.m
@@ -0,0 +1,60 @@
+%compute_VisMoveIndex_SAT.m
+% 
+
+PRINTDIR = "C:\Users\thoma\Dropbox\SAT-Local\";
+VECDIR = deg2rad(linspace(0, 360, 9));
+EPOCH = {'BL [-300,-200]','VR [+50,+150]','PS [-50,+50]','PR [-50,+50]'};
+nEpoch = 4;
+
+for kk = 34:49
+  fprintf('%s \n', behavData.Session{kk})
+
+  %index units by session, area, etc.
+  idx_Sess = ismember(unitData.SessionID, kk);
+  idx_Area = ismember(unitData.Area, {'FEF'});
+  
+  unitTest = unitData( idx_Area & idx_Sess , : );
+  nUnit = size(unitTest,1);
+  
+  hFig = figure("Visible","off");
+  
+  for uu = 1:nUnit
+    fprintf('%s \n', unitTest.ID{uu})
+  
+    %% Compute spike count X condition X direction
+    [scAcc, scFast] = computeSpikeCount_Search(unitTest(uu,:), behavData(kk,:));
+    scAcc  = cat(1, scAcc,  scAcc(1,:)); %complete the circle for plotting
+    scFast = cat(1, scFast, scFast(1,:));
+    
+    scMG = computeSpikeCount_MG(unitTest(uu,:), behavDataMG(kk,:));
+    scMG = cat(1, scMG, scMG(1,:));
+  
+    %% Plotting
+    MARKERSIZE = 15;
+    rLim = ceil(max([scFast scAcc scMG],[],'all'));
+  
+    for ep = 1:4
+      iPlot = nEpoch*(uu-1) + ep;
+      subplot(nUnit,nEpoch,iPlot, polaraxes); hold on;
+
+      polarplot(VECDIR, scMG(:,ep),   '.-', 'Color',[.2 .2 .2], 'MarkerSize',MARKERSIZE);
+      polarplot(VECDIR, scFast(:,ep), '.-', 'Color',[0 .7 0], 'MarkerSize',MARKERSIZE);
+      polarplot(VECDIR, scAcc(:,ep),  '.-', 'Color','r',   'MarkerSize',MARKERSIZE);
+
+      rlim([0 rLim]); thetaticks([])
+      if (ep > 1); rticklabels([]); end
+      if (uu == nUnit); text(deg2rad(245), 1.2*rLim, EPOCH{ep}); end
+
+    end % for : epoch (ep)
+  
+    title(unitTest.ID(uu))
+  
+  end % for : unit (uu)
+  
+  text(deg2rad(45), 1.1*rLim, 'spike count')
+  ppretty([10,2*nUnit]); drawnow
+  print(PRINTDIR + behavData.Session(kk) + ".tif", '-dtiff'); close(hFig)
+
+end % for : session (sess)
+
+clearvars -except ROOTDIR* behavData* unitData pairData *Noise* *Signal*
