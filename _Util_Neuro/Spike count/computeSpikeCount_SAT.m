8aa7813721111fba544733786b8f84c8f765712f
Thomas Reppert <thomas.reppert@gmail.com>
2023-07-17 17:06:05 -0500

    Update computeSpikeCount() analysis
    computeSpikeCount_SAT() - Low-level function used to compute spike count vs epoch for both Search and MG.
    
    computeSpikeCount_Search() - Specific to the search paradigm (i.e., split on task condition Accurate/Fast)

--- _Util_Neuro/Spike count/computeSpikeCount_SAT.m
+++ _Util_Neuro/Spike count/computeSpikeCount_SAT.m
@@ -1,92 +1,17 @@
-function [ scAcc , scFast , varargout ] = computeSpikeCount_SAT( unitTest , behavTest , outcome , varargin )
-%computeSpikeCount_SAT This function computes spike counts across the four
-%main within-trial time windows (baseline, visual response, post-saccade,
-%post-reward), separately for Fast and Accurate conditions.
-%   Input
-%   outcome - 'Correct' 'ErrChoice' 'ErrTime' 'ErrChoiceOnly' 'ErrTimeOnly'
-% 
-%   Output
-%   scAcc(9x4)  - single neuron spike count (direction X epoch) - Accurate
-%   scFast(9x4) - single neuron spike count (direction X epoch) - Fast
-%   stsc - *single-trial* spike count (condition X direction X epoch)
-% 
-
-MIN_TRIAL_COUNT = 5;
-nDir = 8;
-nEpoch = 4; % Baseline | Post-array | Peri-saccade | Peri-reward
-nTrial = behavTest.NumTrials;
-
-TWIN.BL = [-300 -200]; %[-300,-100]; % baseline (re array)
-TWIN.VR = [ +50 +150]; %[+50, +250]; % post-array (re array)
-TWIN.PS = [ -50  +50]; %[0,   +200]; % peri-saccade (re saccade)
-TWIN.PR = [ -50  +50]; %[0,   +200]; % peri-reward (re reward)
-
-%% Check for valid input
-nRow = size(unitTest,1);
-if (nRow ~= 1); error('Input to computeSpikeCount_SAT() should be a single unit'); end
-
-%% Compute spike counts by epoch
-sc_uu = computeSpikeCount(unitTest, behavTest, TWIN);
-
-%% Index by isolation quality
-if (nargin > 3) %if desired, specify trials with poor isolation
-  idxIso = removeTrials_Isolation(varargin{1}, nTrial);
-else
-  idxIso = removeTrials_Isolation(unitTest.isoSAT{1}, nTrial);
-end
-
-%% Index spike counts by trial condition and outcome
-%index by condition
-idxAcc = ((behavTest.Condition{1} == 1) & ~idxIso);
-idxFast = ((behavTest.Condition{1} == 3) & ~idxIso);
-%index by trial outcome
-idxOutcome = behavTest.(outcome){1};
-
-%% Sort spike counts by condition and direction
-scAcc = NaN(nDir+1,nEpoch); %mean spike counts
-scFast = scAcc;
-stsc.Acc = cell(nDir,1); %single-trial spike counts
-stsc.Fast = stsc.Acc;
-for dd = 1:nDir
-  idxDir = (behavTest.Sacc_Octant{1} == dd);
-  idxAccDir =  (idxAcc & idxOutcome & idxDir);
-  idxFastDir = (idxFast & idxOutcome & idxDir);
-
-  if (sum(idxAccDir) >= MIN_TRIAL_COUNT)
-    stsc.Acc{dd} = sc_uu(idxAccDir,:); %single-trial counts
-    scAcc(dd,:)  = mean(sc_uu(idxAccDir,:)); %mean counts
-  else %not enough trials for this direction
-    stsc.Acc{dd} = NaN(1,nEpoch);
-  end
-  if (sum(idxFastDir) >= MIN_TRIAL_COUNT)
-    stsc.Fast{dd} = sc_uu(idxFastDir,:);
-    scFast(dd,:) = mean(sc_uu(idxFastDir,:));
-  else %not enough trials for this direction
-    stsc.Fast{dd} = NaN(1,nEpoch);
-  end
-end % for : direction (dd)
-scAcc(nDir+1,:)  = scAcc(1,:); %close the circle for plotting
-scFast(nDir+1,:) = scFast(1,:);
-
-if (nargout > 2) %if desired, return single-trial counts
-  varargout{1} = stsc;
-end
-
-end % fxn : computeSpikeCount_SAT()
-
-
-function [ spikeCount ] = computeSpikeCount( unitTest , behavTest , tWin )
+function [ spikeCount ] = computeSpikeCount_SAT( unitTest , behavTest , tWin)
 %computeSpikeCount This function computes trial-by-trial spike counts
 %for the SAT data set, separately for Fast and Accurate conditions.
 % 
 %   Input
-%   unitTest -- Physiology data for a single unit
+%   unitTest -- Physiology data for single unit
 %   behavTest -- Behavioral data for single session
-%   tWin  -- Time windows for computing counts
+%   tWin  -- Time windows for computing counts [BL,VR,PS,PR]
 % 
 %   Output
 %   spikeCount -- Spike counts for epochs [BL,VR,PS,PR]
 % 
+
+if (size(unitTest,1) ~= 1); error('Input to computeSpikeCount_SAT() should be a single unit'); end
 
 spikeCount = NaN(behavTest.NumTrials,4); %[BL,VR,PS,PR]
 
@@ -115,4 +40,4 @@ spikeTimes = cellfun(@(x) x-tRew, spikeTimes, 'UniformOutput',false);
 %compute post-reward spike counts
 spikeCount(:,4) = cellfun(@(x) sum((x > tWin.PR(1)) & (x < tWin.PR(2))), spikeTimes);
 
-end % fxn : computeSpikeCount()
+end % fxn : computeSpikeCount_SAT()
---
+++ _Util_Neuro/Spike count/computeSpikeCount_Search.m
@@ -0,0 +1,77 @@
+function [ scAcc , scFast , varargout ] = computeSpikeCount_Search( unitTest , behavTest , varargin )
+%computeSpikeCount_Search This function computes spike counts for the four
+%main within-trial time windows (baseline, visual response, post-saccade,
+%post-reward), separately for Fast and Accurate conditions.
+% 
+%   Input
+%   unitTest  -- Data for a single neuron
+%   behavTest -- Data for corresponding behavioral recording session
+%   Outcome   -- 'Correct' 'ErrChoice' 'ErrTime' 'ErrChoiceOnly' 'ErrTimeOnly'
+% 
+%   Output
+%   scAcc(8x4)  -- Mean spike count X direction X epoch -- Accurate
+%   scFast(8x4) -- Mean spike count X direction X epoch -- Fast
+%   stsc      -- Single-trial spike count X condition X direction X epoch
+% 
+
+args = getopt(varargin, {{'Outcome=','Correct'}});
+
+if (size(unitTest,1)  ~= 1); error('Input to computeSpikeCount_Search() should be a single unit'); end
+if (size(behavTest,1) ~= 1); error('Input to computeSpikeCount_Search() should be a single behavioral session'); end
+
+nDir = 8;
+nEpoch = 4; % Baseline | Post-array | Peri-saccade | Peri-reward
+
+MIN_TRIAL_COUNT = 5; %min count X direction X condition
+tWin.BL = [-300 -200]; %[-300,-100]; % baseline (re array)
+tWin.VR = [ +50 +150]; %[+50, +250]; % post-array (re array)
+tWin.PS = [ -50  +50]; %[0,   +200]; % peri-saccade (re saccade)
+tWin.PR = [ -50  +50]; %[0,   +200]; % peri-reward (re reward)
+
+%% Compute single-trial spike counts for each epoch
+scSingleTrial = computeSpikeCount_SAT(unitTest, behavTest, tWin);
+
+%% Prepare to index spike counts by trial condition and outcome
+%index by recording (isolation) quality
+idxIso = removeTrials_Isolation(unitTest.isoSAT{1}, behavTest.NumTrials);
+%index by condition
+idxAcc = ((behavTest.Condition{1} == 1) & ~idxIso);
+idxFast = ((behavTest.Condition{1} == 3) & ~idxIso);
+%index by trial outcome
+idxOutcome = behavTest.(args.Outcome){1};
+
+%% Sort spike counts by condition and direction
+scAcc = NaN(nDir,nEpoch); %mean spike counts
+scFast = scAcc;
+stsc.Acc = cell(nDir,1); %single-trial spike counts
+stsc.Fast = stsc.Acc;
+
+for dd = 1:nDir
+  %index by target direction
+  idxDir  = (behavTest.Sacc_Octant{1} == dd);
+  idxDirAcc  = (idxAcc  & idxOutcome & idxDir);
+  idxDirFast = (idxFast & idxOutcome & idxDir);
+
+  %Accurate condition
+  if (sum(idxDirAcc) >= MIN_TRIAL_COUNT)
+    stsc.Acc{dd} = scSingleTrial(idxDirAcc,:); %save single-trial counts
+    scAcc(dd,:)  = mean(scSingleTrial(idxDirAcc,:)); %mean count
+  else %not enough trials for this direction
+    stsc.Acc{dd} = NaN(1,nEpoch);
+  end
+
+  %Fast condition
+  if (sum(idxDirFast) >= MIN_TRIAL_COUNT)
+    stsc.Fast{dd} = scSingleTrial(idxDirFast,:);
+    scFast(dd,:) = mean(scSingleTrial(idxDirFast,:));
+  else
+    stsc.Fast{dd} = NaN(1,nEpoch);
+  end
+
+end % for : direction (dd)
+
+if (nargout > 2) %if desired, return single-trial counts
+  varargout{1} = stsc;
+end
+
+end % fxn : computeSpikeCount_Search()
