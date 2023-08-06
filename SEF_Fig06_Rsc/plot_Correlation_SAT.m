217fc5bced45e50d013aa47bcfa34dc2d8c2581b
treppert <thomas.reppert@gmail.com>
2023-08-06 11:05:10 -0400

    Updated analysis of noise correlations

--- SEF_Fig06_Rsc/plot_Correlation_SAT.m
+++ SEF_Fig06_Rsc/plot_Correlation_SAT.m
@@ -1,14 +1,16 @@
-%plot_Correlation_SAT.m
+% plot_Correlation_SAT.m
+% 
+ROOTDIR_CORR = 'C:\Users\thoma\Dropbox\SAT-Local\Analysis - Correlation SEF-SC SEF-FEF\';
 
 %load signal correlation values
-rSignal.SC = readmatrix(ROOTDIR_SAT+"Correlation-SAT-Mat.xlsx", "Sheet","SEF-SC", "Range","D5:G49");
-rSignal.FEF = readmatrix(ROOTDIR_SAT+"Correlation-SAT-Mat.xlsx", "Sheet","SEF-FEF", "Range","D5:G74");
+rSignal.SC = readmatrix(ROOTDIR_CORR+"Correlation-SAT-Mat.xlsx", "Sheet","SEF-SC", "Range","D5:G49");
+rSignal.FEF = readmatrix(ROOTDIR_CORR+"Correlation-SAT-Mat.xlsx", "Sheet","SEF-FEF", "Range","D5:G74");
 rabsSignal.SC = abs(rSignal.SC);
 rabsSignal.FEF = abs(rSignal.FEF);
 
 %load noise correlation values
-rNoise.SC = readmatrix(ROOTDIR_SAT+"Correlation-SAT-Mat.xlsx", "Sheet","SEF-SC", "Range","I5:P49");
-rNoise.FEF = readmatrix(ROOTDIR_SAT+"Correlation-SAT-Mat.xlsx", "Sheet","SEF-FEF", "Range","I5:P74");
+rNoise.SC = readmatrix(ROOTDIR_CORR+"Correlation-SAT-Mat.xlsx", "Sheet","SEF-SC", "Range","I5:P49");
+rNoise.FEF = readmatrix(ROOTDIR_CORR+"Correlation-SAT-Mat.xlsx", "Sheet","SEF-FEF", "Range","I5:P74");
 rabsNoise.SC = abs(rNoise.SC);
 rabsNoise.FEF = abs(rNoise.FEF);
 
@@ -36,59 +38,52 @@ FACEALPHA = 0.5;
 TXTFORMAT = "%2.1f";
 
 %% Plot - Scatter - Signal correlation
-if (false)
-figure()
-subplot(1,2,1); title("VR epoch"); hold on
-scatter(rSignal.SC(:,1),  rSignal.SC(:,3), 20, "magenta", "filled", "o", "MarkerFaceAlpha",FACEALPHA)
-scatter(rSignal.FEF(:,1), rSignal.FEF(:,3), 20, "blue", "filled", "o", "MarkerFaceAlpha",FACEALPHA)
-plot([-1 +1], [-1 +1], 'k-', "LineWidth",0.5)
-xlabel("Signal corr - Accurate"); xtickformat(TXTFORMAT)
-ylabel("Signal corr - Fast"); ytickformat(TXTFORMAT)
-
-subplot(1,2,2); title("PS epoch"); hold on
-scatter(rSignal.SC(:,2),  rSignal.SC(:,4), 20, "magenta", "filled", "o", "MarkerFaceAlpha",FACEALPHA)
-scatter(rSignal.FEF(:,2), rSignal.FEF(:,4), 20, "blue", "filled", "o", "MarkerFaceAlpha",FACEALPHA)
-plot([-1 +1], [-1 +1], 'k-', "LineWidth",0.5)
-xlabel("Signal corr - Accurate"); xtickformat(TXTFORMAT)
-ytickformat(TXTFORMAT)
-ppretty([5.4,2]); drawnow
-end
+% figure()
+% subplot(1,2,1); title("VR epoch"); hold on
+% scatter(rSignal.SC(:,1),  rSignal.SC(:,3), 20, "magenta", "filled", "o", "MarkerFaceAlpha",FACEALPHA)
+% scatter(rSignal.FEF(:,1), rSignal.FEF(:,3), 20, "blue", "filled", "o", "MarkerFaceAlpha",FACEALPHA)
+% plot([-1 +1], [-1 +1], 'k-', "LineWidth",0.5)
+% xlabel("Signal corr - Accurate"); xtickformat(TXTFORMAT)
+% ylabel("Signal corr - Fast"); ytickformat(TXTFORMAT)
+% 
+% subplot(1,2,2); title("PS epoch"); hold on
+% scatter(rSignal.SC(:,2),  rSignal.SC(:,4), 20, "magenta", "filled", "o", "MarkerFaceAlpha",FACEALPHA)
+% scatter(rSignal.FEF(:,2), rSignal.FEF(:,4), 20, "blue", "filled", "o", "MarkerFaceAlpha",FACEALPHA)
+% plot([-1 +1], [-1 +1], 'k-', "LineWidth",0.5)
+% xlabel("Signal corr - Accurate"); xtickformat(TXTFORMAT)
+% ytickformat(TXTFORMAT)
+% ppretty([5.4,2]); drawnow
 
 %% Plot - Scatter - Noise correlation
-if (false)
-rNoiseSC_Acc  = mean(rNoise.SC(:,1:4),2);
-rNoiseSC_Fast = mean(rNoise.SC(:,5:8),2);
-rNoiseFEF_Acc  = mean(rNoise.FEF(:,1:4),2);
-rNoiseFEF_Fast = mean(rNoise.FEF(:,5:8),2);
-
-figure(); hold on
-scatter(rNoiseSC_Acc,  rNoiseSC_Fast,  20, "magenta", "filled", "o", "MarkerFaceAlpha",FACEALPHA)
-scatter(rNoiseFEF_Acc, rNoiseFEF_Fast, 20, "blue", "filled", "o", "MarkerFaceAlpha",FACEALPHA)
-plot([-.2 +.4], [-.2 +.4], 'k-', "LineWidth",0.5)
-xlabel("Noise corr - Accurate"); xtickformat(TXTFORMAT)
-ylabel("Noise corr - Fast"); ytickformat(TXTFORMAT)
-ppretty([2.4,2]); drawnow
-end
+% rNoiseSC_Acc  = mean(rNoise.SC(:,1:4),2);
+% rNoiseSC_Fast = mean(rNoise.SC(:,5:8),2);
+% rNoiseFEF_Acc  = mean(rNoise.FEF(:,1:4),2);
+% rNoiseFEF_Fast = mean(rNoise.FEF(:,5:8),2);
+% 
+% figure(); hold on
+% scatter(rNoiseSC_Acc,  rNoiseSC_Fast,  20, "magenta", "filled", "o", "MarkerFaceAlpha",FACEALPHA)
+% scatter(rNoiseFEF_Acc, rNoiseFEF_Fast, 20, "blue", "filled", "o", "MarkerFaceAlpha",FACEALPHA)
+% plot([-.2 +.4], [-.2 +.4], 'k-', "LineWidth",0.5)
+% xlabel("Noise corr - Accurate"); xtickformat(TXTFORMAT)
+% ylabel("Noise corr - Fast"); ytickformat(TXTFORMAT)
+% ppretty([2.4,2]); drawnow
 
 %% Plot - Scatter - Signal vs noise correlation
-if (false)
-figure()
-subplot(2,2,3); title("Accurate"); hold on
-scatter(rSignal.SC(:,2),  rNoiseSC_Acc,  20, "magenta", "filled", "o", "MarkerFaceAlpha",FACEALPHA)
-scatter(rSignal.FEF(:,2), rNoiseFEF_Acc, 20, "blue", "filled", "o", "MarkerFaceAlpha",FACEALPHA)
-xlabel("Signal corr - PS"); xtickformat(TXTFORMAT)
-ylabel("Noise corr"); ytickformat(TXTFORMAT)
-
-subplot(2,2,4); title("Fast"); hold on
-scatter(rSignal.SC(:,4),  rNoiseSC_Fast,  20, "magenta", "filled", "o", "MarkerFaceAlpha",FACEALPHA)
-scatter(rSignal.FEF(:,4), rNoiseFEF_Fast, 20, "blue", "filled", "o", "MarkerFaceAlpha",FACEALPHA)
-xlabel("Signal corr - PS"); xtickformat(TXTFORMAT)
-ytickformat(TXTFORMAT)
-ppretty([5.4,4.8]); drawnow
-end
+% figure()
+% subplot(2,2,3); title("Accurate"); hold on
+% scatter(rSignal.SC(:,2),  rNoiseSC_Acc,  20, "magenta", "filled", "o", "MarkerFaceAlpha",FACEALPHA)
+% scatter(rSignal.FEF(:,2), rNoiseFEF_Acc, 20, "blue", "filled", "o", "MarkerFaceAlpha",FACEALPHA)
+% xlabel("Signal corr - PS"); xtickformat(TXTFORMAT)
+% ylabel("Noise corr"); ytickformat(TXTFORMAT)
+% 
+% subplot(2,2,4); title("Fast"); hold on
+% scatter(rSignal.SC(:,4),  rNoiseSC_Fast,  20, "magenta", "filled", "o", "MarkerFaceAlpha",FACEALPHA)
+% scatter(rSignal.FEF(:,4), rNoiseFEF_Fast, 20, "blue", "filled", "o", "MarkerFaceAlpha",FACEALPHA)
+% xlabel("Signal corr - PS"); xtickformat(TXTFORMAT)
+% ytickformat(TXTFORMAT)
+% ppretty([5.4,4.8]); drawnow
 
 %% Plot - Fraction positive (signal correlation)
-if (false)
 figure()
 subplot(1,2,1); title("SEF-SC"); hold on % SEF-SC
 bar(frPosSignal.SC, 0.6, "c", "EdgeColor","none"); yline(0.5, 'k')
@@ -101,13 +96,8 @@ xticks(1:4); xticklabels({'VR','PS','VR','PS'})
 ytickformat('%2.1f')
 
 ppretty([5,1.2]); drawnow
-end
 
 %% Plot - Fraction positive (noise correlation)
-if (false)
-xAcc = 1:4;
-xFast = 5:8;
-
 figure()
 subplot(1,2,1); title("SEF-SC"); hold on % SEF-SC
 bar(frPosNoise.SC, 0.5, "m", "EdgeColor","none"); yline(0.5, 'k')
@@ -120,52 +110,50 @@ xticks(1:8); xticklabels({'BL','VR','PS','PR','BL','VR','PS','PR'})
 ytickformat('%2.1f')
 
 ppretty([5,1.2]); drawnow
-end
 
 %% Plot - Absolute value of signal correlation
-if (false)
-figure()
-subplot(1,2,1); title("SEF-SC"); hold on % SEF-SC
-bar(rabsSignal.Mean.SC, 0.6, "c", "EdgeColor","none")
-errorbar(1:2, rabsSignal.Mean.SC(1:2), rabsSignal.SD.SC(1:2), "k", "CapSize",0) %Accurate
-errorbar(3:4, rabsSignal.Mean.SC(3:4), rabsSignal.SD.SC(3:4), "k", "CapSize",0) %Fast
-xticks(1:4); xticklabels({'VR','PS','VR','PS'})
-ylabel('Absolute signal correlation'); ytickformat('%2.1f')
+% figure()
+% subplot(1,2,1); title("SEF-SC"); hold on % SEF-SC
+% bar(rabsSignal.Mean.SC, 0.6, "c", "EdgeColor","none")
+% errorbar(1:2, rabsSignal.Mean.SC(1:2), rabsSignal.SD.SC(1:2), "k", "CapSize",0) %Accurate
+% errorbar(3:4, rabsSignal.Mean.SC(3:4), rabsSignal.SD.SC(3:4), "k", "CapSize",0) %Fast
+% xticks(1:4); xticklabels({'VR','PS','VR','PS'})
+% ylabel('Absolute signal correlation'); ytickformat('%2.1f')
+% 
+% subplot(1,2,2); title("SEF-FEF"); hold on % SEF-FEF
+% bar(rabsSignal.Mean.FEF, 0.6, "c", "EdgeColor","none")
+% errorbar(1:2, rabsSignal.Mean.FEF(1:2), rabsSignal.SD.FEF(1:2), "k", "CapSize",0) %Accurate
+% errorbar(3:4, rabsSignal.Mean.FEF(3:4), rabsSignal.SD.FEF(3:4), "k", "CapSize",0) %Fast
+% xticks(1:4); xticklabels({'VR','PS','VR','PS'})
+% ytickformat('%2.1f')
+% 
+% subplot(1,2,1); ylim([0 0.7])
+% subplot(1,2,2); ylim([0 0.7])
+% 
+% ppretty([5,1.2]); drawnow
 
-subplot(1,2,2); title("SEF-FEF"); hold on % SEF-FEF
-bar(rabsSignal.Mean.FEF, 0.6, "c", "EdgeColor","none")
-errorbar(1:2, rabsSignal.Mean.FEF(1:2), rabsSignal.SD.FEF(1:2), "k", "CapSize",0) %Accurate
-errorbar(3:4, rabsSignal.Mean.FEF(3:4), rabsSignal.SD.FEF(3:4), "k", "CapSize",0) %Fast
-xticks(1:4); xticklabels({'VR','PS','VR','PS'})
-ytickformat('%2.1f')
-
-subplot(1,2,1); ylim([0 0.7])
-subplot(1,2,2); ylim([0 0.7])
-
-ppretty([5,1.2]); drawnow
-end
 %% Plot - Absolute value of noise correlation
-if (false)
-xAcc = 1:4;
-xFast = 5:8;
+% xAcc = 1:4;
+% xFast = 5:8;
+% 
+% figure()
+% subplot(1,2,1); title("SEF-SC"); hold on % SEF-SC
+% bar(rabsNoise.Mean.SC, 0.5, "m", "EdgeColor","none")
+% errorbar(xAcc,  rabsNoise.Mean.SC(xAcc),  rabsNoise.SD.SC(xAcc), "k", "CapSize",0) %Accurate
+% errorbar(xFast, rabsNoise.Mean.SC(xFast), rabsNoise.SD.SC(xFast), "k", "CapSize",0) %Fast
+% xticks(1:8); xticklabels({'BL','VR','PS','PR','BL','VR','PS','PR'})
+% ylabel('Absolute noise correlation'); ytickformat('%2.1f')
+% 
+% subplot(1,2,2); title("SEF-FEF"); hold on % SEF-FEF
+% bar(rabsNoise.Mean.FEF, 0.5, "m", "EdgeColor","none")
+% errorbar(xAcc,  rabsNoise.Mean.FEF(xAcc),  rabsNoise.SD.FEF(xAcc), "k", "CapSize",0) %Accurate
+% errorbar(xFast, rabsNoise.Mean.FEF(xFast), rabsNoise.SD.FEF(xFast), "k", "CapSize",0) %Fast
+% xticks(1:8); xticklabels({'BL','VR','PS','PR','BL','VR','PS','PR'})
+% ytickformat('%2.1f')
+% 
+% subplot(1,2,1); ylim([0 0.3])
+% subplot(1,2,2); ylim([0 0.3])
+% 
+% ppretty([5,1.2]); drawnow
 
-figure()
-subplot(1,2,1); title("SEF-SC"); hold on % SEF-SC
-bar(rabsNoise.Mean.SC, 0.5, "m", "EdgeColor","none")
-errorbar(xAcc,  rabsNoise.Mean.SC(xAcc),  rabsNoise.SD.SC(xAcc), "k", "CapSize",0) %Accurate
-errorbar(xFast, rabsNoise.Mean.SC(xFast), rabsNoise.SD.SC(xFast), "k", "CapSize",0) %Fast
-xticks(1:8); xticklabels({'BL','VR','PS','PR','BL','VR','PS','PR'})
-ylabel('Absolute noise correlation'); ytickformat('%2.1f')
-
-subplot(1,2,2); title("SEF-FEF"); hold on % SEF-FEF
-bar(rabsNoise.Mean.FEF, 0.5, "m", "EdgeColor","none")
-errorbar(xAcc,  rabsNoise.Mean.FEF(xAcc),  rabsNoise.SD.FEF(xAcc), "k", "CapSize",0) %Accurate
-errorbar(xFast, rabsNoise.Mean.FEF(xFast), rabsNoise.SD.FEF(xFast), "k", "CapSize",0) %Fast
-xticks(1:8); xticklabels({'BL','VR','PS','PR','BL','VR','PS','PR'})
-ytickformat('%2.1f')
-
-subplot(1,2,1); ylim([0 0.3])
-subplot(1,2,2); ylim([0 0.3])
-
-ppretty([5,1.2]); drawnow
-end
+clearvars -except ROOTDIR* behavData* unitData* pairData*
--- SEF_Fig06_Rsc/plot_NoiseCorr_X_FxnClass.m
+++ SEF_Fig06_Rsc/plot_NoiseCorr_X_FxnClass.m
@@ -1,10 +1,8 @@
-% function [  ] = plot_NoiseCorr_X_FxnClass( pairData )
-%plot_NoiseCorr_X_FxnClass Visualize noise correlations vs functional
-%classification of neurons in SEF, FEF, and SC.
-%   Detailed explanation goes here
+% plot_NoiseCorr_X_FxnClass.m
+% Visualize noise correlations vs functional classification of neurons in SEF, FEF, and SC.
 % 
 
-MONKEY = 'Da';
+MONKEY = 'Eu';
 XAREA = 'SEF';
 YAREA = 'SC';
 
@@ -17,89 +15,156 @@ idxYArea = ismember(pairData.YArea, YAREA);
 idxXFxn  = ismember(pairData.X_VR, +1);
 idxYFxn  = ismember(pairData.Y_VR, +1);
 
-% pairData = pairData(idxXArea & idxYArea & idxXFxn & idxYFxn, :);
-pairData = pairData(idxXArea & idxYArea, :);
+pairData = pairData(idxXArea & idxYArea & idxXFxn & idxYFxn, :);
+% pairData = pairData(idxXArea & idxYArea, :);
 nPair = size(pairData,1);
 
+% %retrieve RF information
+% for pp = 1:nPair
+%   uX = pairData.XUnit(pp);
+%   uY = pairData.YUnit(pp);
+%   pairData.X_VRF(pp) = unitData.VRF(uX);
+%   pairData.Y_VRF(pp) = unitData.VRF(uY);
+% 
+%   %determine whether RFs overlap
+%   if any(ismember(pairData.X_VRF{pp}, pairData.Y_VRF{pp}))
+%     pairData.RFoverlap(pp) = true;
+%   else
+%     pairData.RFoverlap(pp) = false;
+%   end
+% end
+% 
+% %index by RF overlap
+% idxOverlap = pairData.RFoverlap;
+% pairData(~idxOverlap, :) = [];
+% nPair = size(pairData,1);
 
-%% Plotting - Mean correlation
-% PRINTDIR = 'C:\Users\thoma\Dropbox\SAT-Local\Figures\';
 GREEN = [0 .7 0];
 BARWIDTH = 0.6;
 LINEWIDTH = 1.4;
 
+%% Plotting - Mean correlation
 %average across epochs
 rAC = mean(pairData.rAC,2);
-rFC = mean(pairData.rFC,2, "omitnan");
+rFC = mean(pairData.rFC,2);
 rAE = mean(pairData.rAET,2, "omitnan");
 rFE = mean(pairData.rFEC,2, "omitnan");
 
+%average across pairs
 rACmu = mean(rAC,1);  seAC = std(rAC,0,1) / sqrt(nPair);
 rAEmu = mean(rAE,1);  seAE = std(rAE,0,1) / sqrt(nPair);
 rFCmu = mean(rFC,1);  seFC = std(rFC,0,1) / sqrt(nPair);
 rFEmu = mean(rFE,1);  seFE = std(rFE,0,1) / sqrt(nPair);
 
 %stats
-rNoise = [rAC; rAE; rFC; rFE];
-Condition = [ones(2*nPair,1); 2*ones(2*nPair,1)];
-Outcome = [ones(nPair,1); 2*ones(nPair,1); ones(nPair,1); 2*ones(nPair,1)];
-pNoise = anovan(rNoise,{Condition,Outcome}, 'display','on', 'model','interaction', ...
-  'varnames',{'Condition','Outcome'});
+rNoise = mean([rAC rFC rAE rFE], 2);
+% rNoise = [rAC; rAE; rFC; rFE];
+% Condition = [ones(2*nPair,1); 2*ones(2*nPair,1)];
+% Outcome = [ones(nPair,1); 2*ones(nPair,1); ones(nPair,1); 2*ones(nPair,1)];
+% pNoise = anovan(rNoise,{Condition,Outcome}, 'display','on', 'model','interaction', ...
+%   'varnames',{'Condition','Outcome'});
 
-hFig = figure("Visible","on"); hold on
+%plotting only correct trials
+% hFig = figure("Visible","on"); hold on %only correct trials
+% bar(1, rACmu, BARWIDTH, 'FaceColor','r',   'EdgeColor','none', 'FaceAlpha',0.5)
+% bar(2, rFCmu, BARWIDTH, 'FaceColor',GREEN, 'EdgeColor','none', 'FaceAlpha',0.5)
+% errorbar([1 2], [rACmu rFCmu], [seAC seFC], 'LineWidth',LINEWIDTH, 'Color','k', 'CapSize',0)
 
-bar(1:2, [rACmu rAEmu], BARWIDTH, 'FaceColor','r', 'EdgeColor','none', 'FaceAlpha',0.5)
-errorbar(1:2, [rACmu rAEmu], [seAC seAE], 'LineWidth',LINEWIDTH, 'Color','k', 'CapSize',0)
+%plotting correct and error trials
+% bar(1:2, [rACmu rAEmu], BARWIDTH, 'FaceColor','r', 'EdgeColor','none', 'FaceAlpha',0.5)
+% errorbar(1:2, [rACmu rAEmu], [seAC seAE], 'LineWidth',LINEWIDTH, 'Color','k', 'CapSize',0)
+% bar(3:4, [rFCmu rFEmu], BARWIDTH, 'FaceColor',GREEN, 'EdgeColor','none', 'FaceAlpha',0.5)
+% errorbar(3:4, [rFCmu rFEmu], [seFC seFE], 'LineWidth',LINEWIDTH, 'Color','k', 'CapSize',0)
 
-bar(3:4, [rFCmu rFEmu], BARWIDTH, 'FaceColor',GREEN, 'EdgeColor','none', 'FaceAlpha',0.5)
-errorbar(3:4, [rFCmu rFEmu], [seFC seFE], 'LineWidth',LINEWIDTH, 'Color','k', 'CapSize',0)
-
-% YLINE = .06;
-% plot(1:2, [YLINE YLINE], 'k-', 'LineWidth',LINEWIDTH)
-% plot(3:4, [YLINE YLINE], 'k-', 'LineWidth',LINEWIDTH)
-
-xticks(1:4); xticklabels([]); xlim([0.3 4.7])
-ytickformat('%3.2f'); %ylabel('Noise correlation');
+% xticks(1:4); xticklabels([]); xlim([0.3 4.7])
+% ytickformat('%3.2f'); %ylabel('Noise correlation');
 fprintf(MONKEY + "   " + XAREA + "-" + YAREA + "   FXN-FXN   n = " + num2str(nPair) + "\n")
 
-ppretty([2.6,2]); drawnow
-set(gca, 'XMinorTick','off')
-
-% print(PRINTDIR + unitTest.ID(uu) + ".tif", '-dtiff'); close(hFig)
-% clearvars -except ROOTDIR* behavData* unitData* pairData*
-
-return
+% ppretty([2.6,2]); drawnow
+% set(gca, 'XMinorTick','off')
 
 %% Plotting - Correlation x epoch
-EPOCH = {'BL' 'VR' 'PS' 'PR'}; %time intervals of interest
-
-rAC = mean(pairData.rAC,1);     seAC = std(pairData.rAC,0,1) / sqrt(nPair);
-rFC = mean(pairData.rFC,1);     seFC = std(pairData.rFC,0,1) / sqrt(nPair);
-rAET = mean(pairData.rAET,1);   seAET = std(pairData.rAET,0,1) / sqrt(nPair);
-rFEC = mean(pairData.rFEC,1);   seFEC = std(pairData.rFEC,0,1) / sqrt(nPair);
-
-hFig = figure("Visible","on"); hold on
-xTickAcc = 1:4;
-xTickFast = 6:9;
-xOffset = 0.05;
-
-bar(xTickAcc-xOffset,  rAC, BARWIDTH, 'FaceColor','r',   'EdgeColor','none', 'FaceAlpha',0.5)
-bar(xTickFast-xOffset, rFC, BARWIDTH, 'FaceColor',GREEN, 'EdgeColor','none', 'FaceAlpha',0.5)
-
-errorbar(xTickFast-xOffset, rFC,  seFC,  'Linestyle','-',  'LineWidth',LINEWIDTH, 'Color','k', 'CapSize',0)
-errorbar(xTickFast+xOffset, rFEC, seFEC, 'Linestyle','--',  'LineWidth',LINEWIDTH, 'Color','k', 'CapSize',0)
-errorbar(xTickAcc-xOffset,  rAC,  seAC,  'Linestyle','-',  'LineWidth',LINEWIDTH,  'Color','k', 'CapSize',0)
-errorbar(xTickAcc+xOffset,  rAET, seAET, 'Linestyle',':',  'LineWidth',LINEWIDTH, 'Color','k', 'CapSize',0)
+% EPOCH = {'BL' 'VR' 'PS' 'PR'}; %time intervals of interest
+% 
+% rAC = mean(pairData.rAC,1);     seAC = std(pairData.rAC,0,1) / sqrt(nPair);
+% rFC = mean(pairData.rFC,1);     seFC = std(pairData.rFC,0,1) / sqrt(nPair);
+% rAET = mean(pairData.rAET,1);   seAET = std(pairData.rAET,0,1) / sqrt(nPair);
+% rFEC = mean(pairData.rFEC,1);   seFEC = std(pairData.rFEC,0,1) / sqrt(nPair);
+% 
+% hFig = figure("Visible","on"); hold on
+% xTickAcc = 1:4;
+% xTickFast = 6:9;
+% xOffset = 0.05;
+% 
+% bar(xTickAcc-xOffset,  rAC, BARWIDTH, 'FaceColor','r',   'EdgeColor','none', 'FaceAlpha',0.5)
+% bar(xTickFast-xOffset, rFC, BARWIDTH, 'FaceColor',GREEN, 'EdgeColor','none', 'FaceAlpha',0.5)
+% 
+% errorbar(xTickFast-xOffset, rFC,  seFC,  'Linestyle','-',  'LineWidth',LINEWIDTH, 'Color','k', 'CapSize',0)
+% errorbar(xTickFast+xOffset, rFEC, seFEC, 'Linestyle','--',  'LineWidth',LINEWIDTH, 'Color','k', 'CapSize',0)
+% errorbar(xTickAcc-xOffset,  rAC,  seAC,  'Linestyle','-',  'LineWidth',LINEWIDTH,  'Color','k', 'CapSize',0)
+% errorbar(xTickAcc+xOffset,  rAET, seAET, 'Linestyle',':',  'LineWidth',LINEWIDTH, 'Color','k', 'CapSize',0)
 % legend({'','','Correct','Choice error','','Timing error'}, 'Location','northwest')
+% 
+% xticks([xTickAcc xTickFast]); xticklabels([])
+% ytickformat('%3.2f'); %ylabel('Noise correlation');
+% fprintf(MONKEY + "   " + XAREA + "-" + YAREA + "   FXN-FXN   n = " + num2str(nPair) + "\n")
+% 
+% ppretty([6,2]); drawnow; yline(0)
+% set(gca, 'XMinorTick','off')
+% 
+% % print(PRINTDIR + unitTest.ID(uu) + ".tif", '-dtiff'); close(hFig)
 
-xticks([xTickAcc xTickFast]);  xticklabels([EPOCH EPOCH])
-ylabel('Noise correlation');   ytickformat('%3.2f')
-title(MONKEY + " - SEF-SEF - PS-PS - n = " + num2str(nPair))
+%% Control analysis - Correlation vs mean firing rate
+% rAC = pairData.rAC(:,1); %baseline epoch
+% rFC = pairData.rFC(:,1);
+% scAC = NaN(nPair,2); %[uX,uY]
+% scFC = scAC;
+% 
+% for pp = 1:nPair
+%   uX = pairData.XUnit(pp);
+%   uY = pairData.YUnit(pp);
+%   kk = pairData.SID(pp);
+% 
+%   [scX_Acc,scX_Fast] = computeSpikeCount_Search(unitData(uX,:), behavData(kk,:), 'Outcome','Correct');
+%   [scY_Acc,scY_Fast] = computeSpikeCount_Search(unitData(uY,:), behavData(kk,:), 'Outcome','Correct');
+%   scAC(pp,:) = [mean(scX_Acc(:,1))  , mean(scY_Acc(:,1)) ];
+%   scFC(pp,:) = [mean(scX_Fast(:,1)) , mean(scY_Fast(:,1))];
+% 
+% end
+% 
+% cMap = parula;
+% rBinLim = linspace(-.8, +.8, 257);
+% nColorBin = 256; %standard colormap
+% 
+% cPlotAcc = NaN(nPair,3);
+% cPlotFast = cPlotAcc;
+% for pp = 1:nPair
+%   idxAcc  = find(rAC(pp) > rBinLim, 1, "last");
+%   idxFast = find(rFC(pp) > rBinLim, 1, "last");
+% 
+%   cPlotAcc(pp,:)  = cMap(idxAcc,:);
+%   cPlotFast(pp,:) = cMap(idxFast,:);
+% end
+% 
+% hFig = figure("Visible","on");
+% MARKERSIZE = 40;
+% FACEALPHA = 0.6;
+% 
+% subplot(1,3,1); hold on; title('Accurate')
+% scatter(scAC(:,1), scAC(:,2), MARKERSIZE, cPlotAcc, 'filled', ...
+%   'MarkerFaceAlpha',FACEALPHA)
+% xlabel(['BL spike count ', XAREA])
+% ylabel(['BL spike count ', YAREA])
+% 
+% subplot(1,3,2); hold on; title('Fast')
+% scatter(scFC(:,1), scFC(:,2), MARKERSIZE, cPlotFast, 'filled', ...
+%   'MarkerFaceAlpha',FACEALPHA)
+% xlabel(['BL spike count ', XAREA])
+% 
+% subplot(1,3,3); hold on
+% colormap(cMap); colorbar; clim([-.8 +.8])
+% 
+% ppretty([8,1.8]); drawnow
+% subplot(1,3,3); set(gca, 'XColor','none'); set(gca, 'YColor','none')
 
-ppretty([6,2]); drawnow
-set(gca, 'XMinorTick','off')
-
-% print(PRINTDIR + unitTest.ID(uu) + ".tif", '-dtiff'); close(hFig)
-clearvars -except ROOTDIR* behavData* unitData* pairData*
-
-% end % fxn : plot_NoiseCorr_X_FxnClass()
+clearvars -except ROOTDIR* behavData* unitData* pairData* rNoise
